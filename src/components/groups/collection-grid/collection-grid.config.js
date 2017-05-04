5c6372d95ea29151415b24d158555525d820a5ee
methodog <sittingbottom@gmail.com>
2017-05-04 17:28:51 +0100

    CP-4 add new Collection Card to Fractal

---
+++ src/components/groups/collection-grid/collection-grid.config.js
@@ -0,0 +1,90 @@
+module.exports = {
+  title: 'Collection grid',
+  label: 'Collection grid',
+  status: 'wip',
+  context: {
+    title_1: 'Latest',
+    title_2: 'Explore the Collections',
+    collectionsLatest: [
+      {
+        collectionUrl: '//www.vam.ac.uk/collections/architecture',
+        collectionTitle: 'Architecture',
+        collectionImage: {
+          240: '//vanda-production-assets.s3.amazonaws.com/2016/04/06/10/36/37/04d16cd0-a70e-40d8-89a7-7783bea528ff/eden.jpg',
+          500: '//vanda-production-assets.s3.amazonaws.com/2016/04/06/10/36/37/087c11b1-dbc0-4313-a4a0-308bdb35ae6d/eden.jpg'
+        }
+      },
+      {
+        collectionUrl: '//www.vam.ac.uk/collections/jewellery',
+        collectionTitle: 'Jewellery',
+        collectionImage: {
+          240: '//vanda-production-assets.s3.amazonaws.com/2016/04/11/14/54/33/07dce556-663a-443f-901d-429c3f2e94d6/thistles.jpg',
+          500: '//vanda-production-assets.s3.amazonaws.com/2016/04/11/14/54/33/f6e3f0af-9d9b-48cd-879f-86a6fc20949d/thistles.jpg'
+        }
+      },
+      {
+        collectionUrl: '//www.vam.ac.uk/collections/glass',
+        collectionTitle: 'Glass',
+        collectionImage: {
+          240: '//vanda-production-assets.s3.amazonaws.com/2017/04/13/14/47/17/91ed0b5c-dbe3-4359-8a12-19d2cb650b58/glass-poster_blue.jpg',
+          500: '//vanda-production-assets.s3.amazonaws.com/2017/04/13/14/47/17/d382395d-d5b4-4190-b5c2-a041bccebdd6/glass-poster_blue.jpg'
+        }
+      },
+      {
+        collectionUrl: '//www.vam.ac.uk/collections/gilbert-collection',
+        collectionTitle: 'The Gilbert Collection',
+        collectionImage: {
+          240: '//vanda-production-assets.s3.amazonaws.com/2016/12/22/15/13/52/f52e5b82-55a4-4bce-8720-ad914e3e0b9f/2009CR4310_poster.jpg',
+          500: '//vanda-production-assets.s3.amazonaws.com/2016/12/22/15/13/52/093a0d4b-ef61-4a3a-8124-c1d6ca993dc0/2009CR4310_poster.jpg'
+        }
+      }
+    ],
+    collections: [
+      {
+        modifiers: ['transparent'],
+        collectionUrl: '//www.vam.ac.uk/collections/gilbert-collection',
+        collectionTitle: 'The Gilbert Collection',
+        collectionImage: {
+          240: '//vanda-production-assets.s3.amazonaws.com/2016/12/22/15/13/52/f52e5b82-55a4-4bce-8720-ad914e3e0b9f/2009CR4310_poster.jpg',
+          500: '//vanda-production-assets.s3.amazonaws.com/2016/12/22/15/13/52/093a0d4b-ef61-4a3a-8124-c1d6ca993dc0/2009CR4310_poster.jpg'
+        }
+      },
+      {
+        modifiers: ['transparent'],
+        collectionUrl: '//www.vam.ac.uk/collections/glass',
+        collectionTitle: 'Glass',
+        collectionImage: {
+          240: '//vanda-production-assets.s3.amazonaws.com/2017/04/13/14/47/17/91ed0b5c-dbe3-4359-8a12-19d2cb650b58/glass-poster_blue.jpg',
+          500: '//vanda-production-assets.s3.amazonaws.com/2017/04/13/14/47/17/d382395d-d5b4-4190-b5c2-a041bccebdd6/glass-poster_blue.jpg'
+        }
+      },
+      {
+        modifiers: ['transparent'],
+        collectionUrl: '//www.vam.ac.uk/collections/jewellery',
+        collectionTitle: 'Jewellery',
+        collectionImage: {
+          240: '//vanda-production-assets.s3.amazonaws.com/2016/04/11/14/54/33/07dce556-663a-443f-901d-429c3f2e94d6/thistles.jpg',
+          500: '//vanda-production-assets.s3.amazonaws.com/2016/04/11/14/54/33/f6e3f0af-9d9b-48cd-879f-86a6fc20949d/thistles.jpg'
+        }
+      },
+      {
+        modifiers: ['transparent'],
+        collectionUrl: '//www.vam.ac.uk/collections/auguste-rodin',
+        collectionTitle: 'Auguste Rodin (1840 – 1917)',
+        collectionImage: {
+          240: '//vanda-production-assets.s3.amazonaws.com/2016/11/28/11/47/14/10a5f499-02df-4c57-b86d-3c58e7eacadc/rodin_poster.jpg',
+          500: '//vanda-production-assets.s3.amazonaws.com/2016/11/28/11/47/14/d94cf6f8-283e-4176-83d8-4f3e147d4994/rodin_poster.jpg'
+        }
+      },
+      {
+        modifiers: ['transparent'],
+        collectionUrl: '//www.vam.ac.uk/collections/architecture',
+        collectionTitle: 'Architecture',
+        collectionImage: {
+          240: '//vanda-production-assets.s3.amazonaws.com/2016/04/06/10/36/37/04d16cd0-a70e-40d8-89a7-7783bea528ff/eden.jpg',
+          500: '//vanda-production-assets.s3.amazonaws.com/2016/04/06/10/36/37/087c11b1-dbc0-4313-a4a0-308bdb35ae6d/eden.jpg'
+        }
+      }
+    ]
+  }
+};
