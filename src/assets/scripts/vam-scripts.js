1967a7f8a304e87b996e57f0a2144be2c2025b50
methodog <sittingbottom@gmail.com>
2017-07-11 17:40:11 +0100

    CP-6 add Toggle Nav

--- src/assets/scripts/vam-scripts.js
+++ src/assets/scripts/vam-scripts.js
@@ -2,5 +2,6 @@ import svg4everybody from 'svg4everybody';
 
 require('../../components/services/object_fit_polyfill/_poly-object-fit.js');
 require('../../components/groups/block-grid/_grid-reveal-more.js');
+require('../../components/blocks/toggle-nav/_toggle-nav.js');
 
 svg4everybody();
