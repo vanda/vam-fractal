339751d9149556e43acd3d26b9bfd753b55c3339
methodog <sittingbottom@gmail.com>
2017-10-06 15:53:16 +0100

    WO-552 add video-thumbnail

--- src/assets/scripts/vam-scripts.js
+++ src/assets/scripts/vam-scripts.js
@@ -3,5 +3,6 @@ import svg4everybody from 'svg4everybody';
 require('../../components/services/object_fit_polyfill/_poly-object-fit.js');
 require('../../components/groups/block-grid/_grid-reveal-more.js');
 require('../../components/blocks/toggle-nav/_toggle-nav.js');
+require('../../components/blocks/video-thumbnail/_video-thumbnail.js');
 
 svg4everybody();
