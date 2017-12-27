7bbdf7cbad1e155bdffd8a0a237eee8b77a111ec
Simon Ashbery <sashbery@ASH-MAC.local>
2017-12-27 15:51:52 +0000

    Renames old search result to auto suggest search results -Creates a new search result block

--- src/components/blocks/search-result/search-result.config.js
+++ src/components/blocks/search-result/search-result.config.js
@@ -4,7 +4,8 @@ module.exports = {
   status: 'wip',
   context: {
     content_type: 'Display',
-    title: 'Juree Kim & Neil Brownsword: V&A Korea UK Ceramics with an exceptionally long title to show to the user',
+    title: 'Contemporary Korean Ceramics',
+    description: 'Bringing together the work of fifteen emerging and established artists from Korea, this display offers a glimpse into contemporary Korean studio ceramic practice. Some are inspired by historical truncates after 2 lines ...',
     image: 'https://vanda-production-assets.s3.amazonaws.com/2017/02/15/12/55/45/01a67413-3cca-454a-ab54-c56b4b62afbb/lockwood-kipling-hero.jpg'
   }
 };
