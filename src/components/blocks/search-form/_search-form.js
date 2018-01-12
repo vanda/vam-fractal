a2ad57fbbe917201a240cfc2b3470b41a6370614
methodog <sittingbottom@gmail.com>
2018-01-12 13:33:05 +0000

    WIP search-form

--- src/components/blocks/search-form/_search-form.js
+++ src/components/blocks/search-form/_search-form.js
@@ -1,19 +1,11 @@
-/* eslint-disable */
-// TODO Create a dynamically expanding coloured block
+// Create a dynamically expanding coloured block
 // That mimics the bahaviour of and underscore as shown
 // In the zepplin designs for this element
 // Standard CSS underlines don't allow for the required
 // spacing between the font and the underline itself.
 
-// const inputField = document.querySelectorAll('.js-search-input-underscore')[0];
+const searchInput = document.querySelector('.js-search-input');
 
-// const resizeWidth = () => {
-//   let underscore = document.createElement("div")
-// }
-
-// if (inputField) {
-//   inputField.oninput = resizeWidth;
-//   // for IE
-//   inputField.onpropertychange = inputField.oninput;
-// }
-  
+searchInput.addEventListener('keypress', (e) => {
+  console.log(e);
+});
