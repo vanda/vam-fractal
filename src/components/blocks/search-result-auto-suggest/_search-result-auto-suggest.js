0254add4d3cd59f6c32dcfe11972f88281ffbe0a
Simon Ashbery <sashbery@ASH-MAC.local>
2018-01-19 11:42:25 +0000

    Fixes js linting -Removes search field -Removes references to search auto suggest result

--- src/components/blocks/search-result-auto-suggest/_search-result-auto-suggest.js
+++ src/components/blocks/search-result-auto-suggest/_search-result-auto-suggest.js
@@ -3,46 +3,47 @@
 // TODO Currently this script doesn't behave correctly
 // Sometimes it truncates after the cutoff
 const textElements = document.querySelectorAll('.js-truncate-text');
-const originalText = {}
-for(const key in textElements) {
-    originalText[key] = textElements[key].innerHTML;
-};
+const originalText = {};
 
-const hasOverflown = (textElement) => {
-    return (textElement.scrollHeight > textElement.clientHeight);
-};
+Array.from(textElements, (textElement) => {
+  originalText.push(textElement.innerHTML);
+  return true;
+});
+
+function hasOverflown (textElement) {
+  return (textElement.scrollHeight > textElement.clientHeight);
+}
 
 const overflowPosition = (textElement) => {
-    const text = textElement.innerHTML;
-    let characterCounter = 0;
-    
-    textElement.removeChild(textElement.firstChild);
-    
-    for(let i = 0; i < text.length; i++) {
-        let newNode = document.createElement('span');
-        newNode.appendChild(document.createTextNode(text.charAt(i)));
-        textElement.appendChild(newNode);
-    
-        if (newNode.offsetLeft < textElement.offsetWidth) {
-            characterCounter++;
-        }
-    
+  const text = textElement.innerHTML;
+  let characterCounter = 0;
+
+  textElement.removeChild(textElement.firstChild);
+
+  for (let i = 0; i < text.length; i += 1) {
+    const newNode = document.createElement('span');
+    newNode.appendChild(document.createTextNode(text.charAt(i)));
+    textElement.appendChild(newNode);
+
+    if (newNode.offsetLeft < textElement.offsetWidth) {
+      characterCounter += 1;
     }
-    return (characterCounter);
+  }
+  return (characterCounter);
 };
 
 const truncateText = () => {
-    textElements.forEach((textElement, index) => {
-        if (hasOverflown (textElement)) {
-            textElement.innerHTML = `${originalText[index].substring(0, (overflowPosition(textElement) - 3))}...`;
-        } else {
-            textElement.innerHTML = originalText[index]
-        }
-    })
+  textElements.forEach((textElement, index) => {
+    if (hasOverflown(textElement)) {
+      textElement.innerHTML = `${originalText[index].substring(0, (overflowPosition(textElement) - 3))}...`;
+    } else {
+      textElement.innerHTML = originalText[index];
+    }
+  });
 };
 
 if (textElements.length > 0) {
-    // TODO Behaviour on dynamic resize isn't working properly, Currently only run on page load
-    window.onload = truncateText()
-    // window.addEventListener('resize', truncateText);
-};
+  // TODO Behaviour on dynamic resize isn't working properly, Currently only run on page load
+  window.onload = truncateText();
+  // window.addEventListener('resize', truncateText);
+}
