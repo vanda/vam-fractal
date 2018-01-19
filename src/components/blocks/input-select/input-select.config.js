0fc85088c85f13d1ce29e225ee6d11b28410790b
methodog <sittingbottom@gmail.com>
2018-01-19 18:01:14 +0000

    input-select

---
+++ src/components/blocks/input-select/input-select.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Input Select',
+  label: 'Input Select',
+  status: 'wip',
+  context: {
+    input_name: 'my_input',
+    input_tooltip: 'Select an option',
+    option_1: 'Option 1',
+    option_2: 'Option 2'
+  }
+};
