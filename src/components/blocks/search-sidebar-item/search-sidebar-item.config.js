8bb9bb0ced328425dfacf9520df2727f2cac92da
Simon Ashbery <sashbery@ASH-MAC.local>
2018-01-23 15:52:10 +0000

    Adds sidebar item component

---
+++ src/components/blocks/search-sidebar-item/search-sidebar-item.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Search Sidebar Item',
+  label: 'Search Sidebar Item',
+  status: 'wip',
+  context: {
+    content_type: 'Display',
+    title: 'Lunchtime Lecture: Ceramics, Speedos truncates after 1 line when > 768 and 2 lines otherwise',
+    description: 'Bringing together the work of fifteen emerging and established artists from Korea, this display offers a glimpse into contemporary Korean studio ceramic practice. Some are inspired by historical truncates after 2 lines normally but after 4 at smaller sizes',
+    image: 'https://vanda-production-assets.s3.amazonaws.com/2017/02/15/12/55/45/01a67413-3cca-454a-ab54-c56b4b62afbb/lockwood-kipling-hero.jpg'
+  }
+};
