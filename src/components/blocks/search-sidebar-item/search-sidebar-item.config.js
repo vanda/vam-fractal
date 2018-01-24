5abf378262705a73e7507f85cf3271600b9591fd
Simon Ashbery <sashbery@ASH-MAC.local>
2018-01-24 14:41:39 +0000

    Adds short varient for search sidebar item

--- src/components/blocks/search-sidebar-item/search-sidebar-item.config.js
+++ src/components/blocks/search-sidebar-item/search-sidebar-item.config.js
@@ -3,9 +3,25 @@ module.exports = {
   label: 'Search Sidebar Item',
   status: 'wip',
   context: {
-    content_type: 'Display',
-    title: 'Lunchtime Lecture: Ceramics, Speedos truncates after 1 line when > 768 and 2 lines otherwise',
-    description: 'Bringing together the work of fifteen emerging and established artists from Korea, this display offers a glimpse into contemporary Korean studio ceramic practice. Some are inspired by historical truncates after 2 lines normally but after 4 at smaller sizes',
     image: 'https://vanda-production-assets.s3.amazonaws.com/2017/02/15/12/55/45/01a67413-3cca-454a-ab54-c56b4b62afbb/lockwood-kipling-hero.jpg'
-  }
+  },
+  variants: [
+    {
+      name: 'default',
+      label: 'Default',
+      context: {
+        default: true,
+        title: 'Shop'
+      }
+    },
+    {
+      name: 'Short',
+      label: 'Short',
+      context: {
+        default: false,
+        modifier: 'short',
+        title: 'V&A museum of Childhood'
+      }
+    }
+  ]
 };
