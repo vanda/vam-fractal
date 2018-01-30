00e1b084ac5dc68359fcda5e4c2b076468a427ce
methodog <sittingbottom@gmail.com>
2018-01-30 16:00:06 +0000

    search result: re-attempt truncation script; style key term

--- src/components/blocks/search-result-auto-suggest/search-result-auto-suggest.config.js
+++ src/components/blocks/search-result-auto-suggest/search-result-auto-suggest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   status: 'wip',
   context: {
     content_type: 'Display',
-    title: 'Juree Kim & Neil Brownsword: V&A Korea UK Ceramics with an exceptionally long title to show to the user',
+    title: 'Juree Kim & Neil Brownsword: V&A Korea UK Ceramics with an exceptionally long <em>title</em> to show to the user',
     image: 'https://vanda-production-assets.s3.amazonaws.com/2017/02/15/12/55/45/01a67413-3cca-454a-ab54-c56b4b62afbb/lockwood-kipling-hero.jpg'
   }
 };
