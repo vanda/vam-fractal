ddc628ecde118355ef9df759b9b23eff00f611ec
Simon Ashbery <sashbery@ASH-MAC.local>
2018-02-01 12:07:24 +0000

    Changes the membership popup to have a fixed position relative to screen space

--- src/components/blocks/membership-upsell/membership-upsell.config.js
+++ src/components/blocks/membership-upsell/membership-upsell.config.js
@@ -7,6 +7,63 @@ module.exports = {
     body: 'Enjoy unlimited free entry to all our exhibitions from as little as £57',
     cta: 'Become a Member',
     continue: 'Continue to booking',
+    /* eslint-disable no-multi-str */
+    filler: 'this is some text to show that the overlay is centered base on screen space\
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.Suspendisse eu luctus massa, eu molestie turpis.Class aptent taciti\
+    sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.Nunc dolor diam, fringilla a ante quis, bibendum\
+    varius eros.Aenean viverra eros non ultrices venenatis.Morbi ultricies pulvinar quam at posuere.Cras mollis auctor cursus.\
+    Nulla vitae ligula neque.Praesent non facilisis mauris.Duis venenatis varius odio in ultrices.Nunc nec lorem tempus, commodo\
+    quam eget, mattis tellus.Sed vel sollicitudin erat.Duis accumsan consequat felis, sit amet porta felis congue quis.In\
+    facilisis tortor mi, a bibendum odio finibus non.Pellentesque facilisis, nulla ornare pharetra placerat, lorem ex volutpat\
+    turpis, sed scelerisque dolor ex quis felis.Suspendisse potenti.Aliquam erat volutpat.Pellentesque blandit neque ut metus\
+    elementum, nec lobortis libero blandit.Donec non ex in nunc semper hendrerit a id nisi.Integer rutrum urna mi, a rutrum\
+    leo tristique in.Vestibulum facilisis felis ut ipsum porttitor, non dignissim erat placerat.Aliquam nec feugiat mi.Sed\
+    sagittis aliquam quam, id congue ante vehicula vel.Praesent massa ex, auctor ac risus non, commodo egestas erat.Donec consequat\
+    eros ut ligula venenatis, ut gravida velit blandit.Vestibulum tincidunt eleifend risus, non luctus leo imperdiet id.Nulla\
+    ac nisi vitae eros finibus porttitor.Nam convallis sed tellus id euismod.Maecenas commodo nulla ac erat facilisis, non\
+    posuere turpis finibus.Nunc mollis auctor dapibus.Nulla magna nisi, viverra facilisis malesuada at, convallis vitae orci.\
+    Maecenas orci lorem, semper sodales felis id, auctor egestas mauris.Pellentesque ex arcu, elementum nec mi sit amet, ullamcorper\
+    posuere tellus.Pellentesque nec mi eu felis vulputate aliquam.Pellentesque euismod, neque eget efficitur rutrum, nisl neque\
+    posuere dui, sed fermentum arcu ipsum id nibh.Pellentesque ut nisl et dui malesuada finibus.Fusce sodales dignissim rutrum.\
+    Praesent bibendum venenatis pulvinar.Vestibulum a ipsum nec eros volutpat posuere.Mauris in egestas nisl.Vestibulum commodo\
+    posuere luctus.Proin maximus tincidunt quam quis congue.Integer vulputate, quam et accumsan rutrum, massa massa sagittis\
+    purus, scelerisque mattis nunc dolor ut dolor.Ut euismod felis vel elit porttitor commodo.Maecenas et consequat massa.\
+    Vivamus fringilla sem nec lorem accumsan, in gravida lectus auctor.Donec sit amet velit a purus pellentesque lobortis auctor\
+    a tellus.Vestibulum feugiat ante nisl, at rutrum elit fermentum egestas.In a augue consequat diam molestie porta.Morbi\
+    ac convallis metus, eu lacinia diam.Etiam sit amet lacus eu diam venenatis ultrices.Ut in scelerisque urna.Donec convallis\
+    laoreet facilisis.Donec eget libero ultricies, iaculis ligula ac, lacinia quam.Phasellus sollicitudin auctor tellus quis\
+    posuere.Maecenas volutpat in purus sed mattis.Mauris vulputate risus vitae efficitur rutrum.Integer faucibus nisl tellus.\
+    Nunc sagittis fringilla ante, eu tincidunt augue elementum a.Morbi id nibh sed lacus hendrerit volutpat.Nam pretium velit\
+    enim, sed rutrum magna consectetur in.Suspendisse sed facilisis nibh, id sodales leo.Duis a ex ut dui convallis fermentum\
+      in at risus.Cras id dolor id erat ultrices tincidunt maximus vel tortor.Praesent ullamcorper interdum interdum.Integer\
+    faucibus lectus ut dui rutrum vulputate non sit amet odio.Cras mollis, nisi nec porttitor lobortis, magna erat semper ipsum,\
+    ut sodales velit nisi vel justo.Maecenas maximus massa ut varius aliquam.Aliquam vel malesuada arcu, ut aliquam sapien.\
+    Vivamus accumsan imperdiet tellus eu commodo.Sed pharetra pretium mattis.Fusce consectetur hendrerit nisi a congue.Suspendisse\
+    pulvinar leo vel consequat commodo.Suspendisse potenti.Phasellus malesuada enim ac lacus rhoncus fringilla.Aliquam laoreet\
+    metus lacus, id sagittis nisi elementum id.Aenean nisi velit, tincidunt non nisl eu, tempor ornare augue.Duis eleifend\
+    sapien pellentesque elit iaculis, non posuere sem sollicitudin.Mauris tempor lectus nisl, in cursus ante blandit in.Suspendisse\
+    pellentesque lorem aliquet ex imperdiet, eget fringilla tortor pellentesque.Praesent tristique, massa non placerat egestas,\
+    est ex semper massa, quis scelerisque tellus quam at elit.Curabitur vel sapien leo.Nam varius blandit massa hendrerit porttitor.\
+    Donec consequat fringilla urna ut vulputate.Integer pulvinar ultricies quam, porttitor varius sapien cursus quis.Nulla\
+    nec turpis ornare arcu auctor congue.Donec vel eros molestie, auctor tellus vitae, facilisis nisl.Nulla ac tempus enim.\
+    Morbi malesuada leo justo, at pulvinar nibh rhoncus eu.In hac habitasse platea dictumst.Ut lorem orci, pulvinar vitae placerat\
+    a, aliquet eget nisi.Aliquam lectus ante, efficitur at rutrum rutrum, finibus nec libero.Donec erat lacus, varius in molestie\
+    vel, vulputate sed lacus.Vivamus non diam tellus.Aliquam efficitur nulla vel ultricies consectetur.Sed congue semper leo.\
+    Aliquam a suscipit sem.Integer pulvinar nulla leo, id euismod purus bibendum vitae.Mauris non ornare nunc.Mauris vitae\
+    sapien sem.Cras sit amet tempor est, in facilisis metus.Duis lacinia nec sem et imperdiet.Etiam rhoncus placerat metus,\
+    a porta libero auctor eu.Nulla fringilla euismod tincidunt.Etiam quam tortor, maximus sed tempus quis, egestas vel ipsum.\
+    Morbi in est venenatis, tristique metus sed, lacinia nisi.Mauris imperdiet consectetur orci non tempus.Phasellus rutrum\
+    sem mi, in hendrerit enim elementum ac.Curabitur eget posuere orci, eu dapibus nunc.Ut blandit egestas risus, id congue\
+    risus convallis sed.Cras sed auctor augue, sed efficitur sapien.Aenean tempor commodo tortor, eget fermentum mauris lacinia\
+    viverra.Pellentesque sed purus eget tortor tristique pulvinar.Morbi eu enim vestibulum, dignissim enim a, accumsan lacus.\
+    Maecenas et lectus hendrerit, posuere ipsum sed, ornare augue.In viverra, orci eget fringilla accumsan, neque odio vehicula\
+    sem, vitae vestibulum dolor nibh mollis urna.Ut nec erat ornare, consequat metus at, sodales mi.Proin urna elit, pharetra\
+    sed nulla vitae, ullamcorper ornare mauris.Mauris ac metus ultricies metus molestie gravida.Ut viverra in diam cursus tincidunt.\
+    Pellentesque et molestie ipsum.Curabitur egestas lectus urna, nec accumsan purus faucibus sit amet.Donec ullamcorper est\
+    at nibh venenatis sollicitudin.Etiam at tellus eu neque hendrerit luctus ac eget nibh.Donec laoreet semper lectus, nec\
+    condimentum augue tempus non.Donec convallis mollis tellus quis ultricies.Nam quis congue dolor.Interdum et malesuada\
+    fames ac ante ipsum primis in faucibus.Ut ut laoreet quam.Nulla facilisi.',
+    /* eslint-enable no-multi-str */
     image: {
       default: 'https://vanda-production-assets.s3.amazonaws.com/static/membership/Membership_popup_background.png',
       mobile: 'https://vanda-production-assets.s3.amazonaws.com/static/membership/Membership_popup_background_mobile.png'
