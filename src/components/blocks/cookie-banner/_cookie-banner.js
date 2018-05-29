daa29810c5830930ef022114f91ffad3f047196c
Simon Ashbery <sashbery@ASH-MAC.local>
2018-05-29 10:40:26 +0100

    Adds max width of 1200px to bookie banner content -Adds transition effect to show and hide cookie banner. -Updates cookie banner script to add and remove transition class

--- src/components/blocks/cookie-banner/_cookie-banner.js
+++ src/components/blocks/cookie-banner/_cookie-banner.js
@@ -2,8 +2,11 @@ const cookieBanner = document.querySelector('.js-cookie-banner');
 
 if (cookieBanner) {
   const closeButton = document.querySelector('.js-cookie-banner-close');
+  window.onload = () => {
+    cookieBanner.classList.remove('b-cookie-banner--hidden');
+  };
   closeButton.onclick = (event) => {
     event.preventDefault();
-    cookieBanner.classList.add('hidden');
+    cookieBanner.classList.add('b-cookie-banner--hidden');
   };
 }
