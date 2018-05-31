c936187a500396974c711a347443e1baf38ee4fc
Simon Ashbery <sashbery@ASH-MAC.local>
2018-05-31 15:54:27 +0100

    adds dark overlay variant

--- src/components/blocks/background-video/background-video.config.js
+++ src/components/blocks/background-video/background-video.config.js
@@ -4,5 +4,15 @@ module.exports = {
   status: 'wip',
   context: {
     video: 'https://vanda-production-assets.s3.amazonaws.com/2018/05/15/10/27/13/a7bed0db-3d0f-4d30-aa0f-2de60d5ed1d1/TFSH.mp4'
-  }
+  },
+  variants: [
+    {
+      name: 'Dark Overlay',
+      label: 'Dark Overlay',
+      context: {
+        video: 'https://vanda-production-assets.s3.amazonaws.com/2018/05/15/10/27/13/a7bed0db-3d0f-4d30-aa0f-2de60d5ed1d1/TFSH.mp4',
+        treatment: 'darken'
+      }
+    }
+  ]
 };
