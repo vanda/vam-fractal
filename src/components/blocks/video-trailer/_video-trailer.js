8754389d4deafa3796cfcac8511f55c003ca138a
Ash <ash@vam09252.home>
2018-06-05 14:35:30 +0100

    (wip)

--- src/components/blocks/video-trailer/_video-trailer.js
+++ src/components/blocks/video-trailer/_video-trailer.js
@@ -1,12 +1,12 @@
 const videoTrailer = document.querySelector('.js-video-trailer');
 console.log(1);
 if (videoTrailer) {
-  console.log(2)
+  console.log(2);
   const previewImage = videoTrailer.querySelector('.js-video-trailer-preview');
   const popup = document.createElement('div');
   const videoHeightRatio = 0.5625;
   videoTrailer.addEventListener('click', (e) => {
-    console.log(3)
+    console.log(3);
     e.preventDefault();
     popup.className = 'b-video-trailer__image';
     popup.innerHTML = `<iframe class="" width="100%" height="100%" src="${videoTrailer.href}"&playsinline=1" frameborder="0" allowfullscreen></iframe>`;
@@ -16,7 +16,7 @@ if (videoTrailer) {
   });
 
   window.addEventListener('resize', () => {
-    console.log(4)
+    console.log(4);
     popup.style.height = `${popup.offsetWidth * videoHeightRatio}px`;
   });
 }
