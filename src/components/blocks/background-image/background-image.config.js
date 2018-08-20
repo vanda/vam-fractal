47b88ea58b3bac923b278ada8bd3f5f7d6b5a73e
Simon Ashbery <sashbery@ASH-MAC.local>
2018-08-20 16:53:34 +0100

    Adds background image block with darker variant -Adds holding page group with ticketing message set as default

---
+++ src/components/blocks/background-image/background-image.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'Background Image',
+  label: 'Background Image',
+  status: 'wip',
+  variants: [
+    {
+      name: 'Darker',
+      label: 'Darker',
+      context: {
+        modifiers: ['darker']
+      }
+    }
+  ]
+};
---
+++ src/components/groups/holding-page/holding-page.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  default: 'promo',
+  title: 'Holding Page',
+  label: 'Holding Page',
+  status: 'wip',
+  context: {
+    heading: 'Please try again later to book your tickets',
+    explaination: 'We are temporarily undergoing maintenance on our ticketing site.',
+    action: 'Please try again later, or call us on +44 (0)20 7942 2000 between Monday and Sunday, 09.30 – 17.30 to make your booking.'
+  }
+};
