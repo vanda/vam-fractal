ac877f109fc00db2750c17307b091b22d6bbea1c
methodog <sittingbottom@gmail.com>
2018-09-19 18:27:02 +0100

    add list unit

---
+++ src/components/units/list/list.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: 'list',
+  label: 'lists',
+  context: {
+    text: 'list item'
+  },
+  variants: [
+    {
+      name: 'unstyled',
+      label: 'Unstyled',
+      context: {
+        modifiers: ['unstyled']
+      }
+    }
+  ]
+};
