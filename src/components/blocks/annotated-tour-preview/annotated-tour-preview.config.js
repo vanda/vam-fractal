46dd5959eb613cb5d12610550c20776a905811bb
methodog <sittingbottom@gmail.com>
2019-05-15 13:52:29 +0100

    annotated tour style fixes

--- src/components/blocks/annotated-tour-preview/annotated-tour-preview.config.js
+++ src/components/blocks/annotated-tour-preview/annotated-tour-preview.config.js
@@ -3,8 +3,11 @@ module.exports = {
   label: 'Annotated Tour Preview',
   status: 'wip',
   context: {
-    image:
-      'https://framemark.vam.ac.uk/collections/2013GU2911/full/full/0/default.jpg',
-    youtubeID: 'qlb7Roty1FQ'
+    image: {
+      320: 'https://framemark.vam.ac.uk/collections/2013GU2911/full/320,/0/default.jpg',
+      640: 'https://framemark.vam.ac.uk/collections/2013GU2911/full/640,/0/default.jpg',
+      960: 'https://framemark.vam.ac.uk/collections/2013GU2911/full/960,/0/default.jpg',
+      1200: 'https://framemark.vam.ac.uk/collections/2013GU2911/full/1200,/0/default.jpg'
+    }
   }
 };
