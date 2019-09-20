2ab5922d908c1c4656213a7c12828fd5ffd69e2e
methodog <sittingbottom@gmail.com>
2019-09-20 13:49:16 +0100

    improve object-fit polyfill to work dynamically when img src changes - necessary for lazyloading, and also more responsive

--- src/components/services/object_fit_polyfill/_poly-object-fit.js
+++ src/components/services/object_fit_polyfill/_poly-object-fit.js
@@ -2,11 +2,14 @@ function polyfillObjectFit () {
   const objectFitContainer = document.querySelectorAll('.js-object-fit-container');
   [].forEach.call(objectFitContainer, (els) => {
     if (els.getElementsByTagName('img')[0]) {
-      const imgUrl = els.getElementsByTagName('img')[0].src;
-      if (imgUrl) {
-        els.classList.add('js-object-fit-container--fallback');
-        els.style.backgroundImage = `url('${imgUrl}')`;
-      }
+      const imgEl = els.getElementsByTagName('img')[0];
+      imgEl.addEventListener('load', () => {
+        const imgUrl = imgEl.src;
+        if (imgUrl) {
+          els.classList.add('js-object-fit-container--fallback');
+          els.style.backgroundImage = `url('${imgUrl}')`;
+        }
+      });
     }
   });
 }
