fbc49c0c1163ea298b84da25214fb83ade9398c3
Patrick Cartlidge <pcartlidge@vam11037.local>
2020-02-04 16:04:34 +0000

    fix this issue later

--- src/components/stc2-image-overlay/image-carosel/_image-carosel.js
+++ src/components/stc2-image-overlay/image-carosel/_image-carosel.js
@@ -15,6 +15,12 @@
 
 	var concealCarosel = document.querySelector(".image-carosel--conceal .image-carosel__image-carosel")
 
+	if (concealCarosel) {
+		/* fix this later */
+		document.querySelector(".image-carosel__conceal-left").onclick = function(){scrollCarosel(140);};
+		document.querySelector(".image-carosel__conceal-right").onclick = function(){scrollCarosel(-140);};		
+	}
+
 	concealCarosel && concealCarosel.addEventListener("scroll", function(e){
 
 		var scrollLeft = e.target.scrollLeft;
