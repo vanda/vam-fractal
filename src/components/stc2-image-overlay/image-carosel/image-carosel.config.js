be03a37e13d421f7a74cb5135980239b5f73ed3c
Patrick Cartlidge <pcartlidge@vam11037.local>
2020-02-04 14:00:44 +0000

    modified padding and added zoom component

---
+++ src/components/stc2-image-overlay/image-carosel/image-carosel.config.js
@@ -0,0 +1,50 @@
+module.exports = {  
+  variants: [
+		{
+      		name: 'default',
+      		label: 'Default',
+      		context: {
+      			images: [
+      				{
+      					src: '',
+      					selected: true
+      				},
+      				{
+      					src: ''
+      				},
+      				{
+      					src: ''
+      				}
+      			]
+      		}
+	    },
+	    {
+	    	name: 'more-than-five-images',
+	    	label: 'More than five images',
+	    	context: {
+	    		modifiers: ['conceal'],
+      			images: [
+      				{
+      					src: '',
+      					selected: true      					
+      				},
+      				{
+      					src: ''
+      				},
+      				{
+      					src: ''
+      				},
+      				{
+      					src: ''
+      				},
+      				{
+      					src: ''
+      				},
+      				{
+      					src: ''
+      				}      				
+      			]
+	    	}
+	    }
+    ]
+}
