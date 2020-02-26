fbb7bdab3d2793a525bc8472fbe3a8fc5fa63c70
Patrick Cartlidge <pcartlidge@vam11037.local>
2020-02-26 17:05:07 +0000

    concealers and revealers

--- src/components/etc/object-details/object-details.js
+++ src/components/etc/object-details/object-details.js
@@ -1,10 +1,24 @@
 (function(){
-	document.onload = function() {
-		Array.from(document.querySelectorAll(".etc-details__cell-free")).forEach(function(e) {
-			console.log('ayyy')
-			if (e.offsetWidth > 200) {
-				e.classList.add('etc-details__cell-free--hidden');
-			}
-		})
-	}
+
+	var revealEl = document.createElement("DIV");
+	revealEl.classList.add("etc-details__cell-concealer");
+	var revealHTML = '<span class="etc-details__cell-concealer-text">\
+                            Read More\
+                        </span>\<button class="etc-details__cell-concealer-button">+</button>'
+	var hideHTML = '<span class="etc-details__cell-concealer-text">\
+                            Read Less\
+                        </span>\<button class="etc-details__cell-concealer-button">-</button>'
+
+	revealEl.innerHTML = revealHTML; 
+    revealEl.onclick = function(e) {
+    	e.target.innerHTML = hideHTML;
+    }
+
+	Array.from(document.querySelectorAll(".etc-details__cell-free")).forEach(function(e) {
+		if (e.offsetWidth > 200) {
+			var content = e.querySelector(".etc-details__cell-free-content");
+			content.classList.add('etc-details__cell-free-content--hidden');
+			e.appendChild(revealEl);
+		}
+	});
 })();
