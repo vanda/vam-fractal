f1229ce10fc6b15e121620e6b8f765c56371f84f
Patrick Cartlidge <pcartlidge@Patricks-MBP.vamwin.int.vam.ac.uk>
2020-03-11 12:33:01 +0000

    can have multiple openers

--- src/components/stc2-image-overlay/image-overlay/_image-overlay.js
+++ src/components/stc2-image-overlay/image-overlay/_image-overlay.js
@@ -5,7 +5,9 @@
 	document.querySelector(".etc-image-overlay__close").onclick = function() {
 		document.querySelector(".etc-image-overlay__container").classList.remove("etc-image-overlay__container--active");
 	}
-	document.querySelector(".etc-image-overlay__preview").onclick = function() {
-		document.querySelector(".etc-image-overlay__container").classList.add("etc-image-overlay__container--active");
-	}	
+	Array.from(document.querySelectorAll(".etc-image-overlay__preview")).forEach(function(e) {
+		e.onclick = function() {
+			document.querySelector(".etc-image-overlay__container").classList.add("etc-image-overlay__container--active");
+		}
+	})	
 })();
