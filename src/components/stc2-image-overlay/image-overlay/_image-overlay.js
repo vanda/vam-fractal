34921f739a3ea2f1b77679955c229247a8ec4258
Patrick Cartlidge <pcartlidge@Patricks-MBP.vamwin.int.vam.ac.uk>
2020-03-12 11:54:10 +0000

    changed to use events;

--- src/components/stc2-image-overlay/image-overlay/_image-overlay.js
+++ src/components/stc2-image-overlay/image-overlay/_image-overlay.js
@@ -6,12 +6,20 @@
 		document.querySelector(".etc-image-overlay__container").classList.remove("etc-image-overlay__container--active");
 		document.querySelector("body").style.overflowY = "auto";
 	}
+
+	function openObjectOverlay() {
+		document.querySelector(".etc-image-overlay__container").classList.add("etc-image-overlay__container--active");
+		document.querySelector("body").style.overflowY = "hidden";
+		var scrollY = window.scrollY;
+		document.querySelector(".etc-image-overlay__container").style.top = String(scrollY) + "px";
+	}
+
+	console.log(document.querySelector(".etc-image-overlay__container"));
+	document.querySelector(".etc-image-overlay").addEventListener("openObjectOverlay", openObjectOverlay);
+
 	Array.from(document.querySelectorAll(".etc-image-overlay__preview")).forEach(function(e) {
-		e.onclick = function() {
-			document.querySelector(".etc-image-overlay__container").classList.add("etc-image-overlay__container--active");
-			document.querySelector("body").style.overflowY = "hidden";
-			var scrollY = window.scrollY;
-			document.querySelector(".etc-image-overlay__container").style.top = String(scrollY) + "px";
+		e.onclick = function(e) {
+			e.target.dispatchEvent(new CustomEvent("openObjectOverlay", {bubbles: true}));
 		}
-	})	
+	})
 })();
