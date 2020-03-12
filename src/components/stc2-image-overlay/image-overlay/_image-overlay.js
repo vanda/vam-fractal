068d4cf4ea5a3e92422f131a661bd7c1ad0901fd
Patrick Cartlidge <pcartlidge@Patricks-MBP.vamwin.int.vam.ac.uk>
2020-03-12 11:03:52 +0000

    modal now works for entire page not just top of page

--- src/components/stc2-image-overlay/image-overlay/_image-overlay.js
+++ src/components/stc2-image-overlay/image-overlay/_image-overlay.js
@@ -4,10 +4,14 @@
 (function(){
 	document.querySelector(".etc-image-overlay__close").onclick = function() {
 		document.querySelector(".etc-image-overlay__container").classList.remove("etc-image-overlay__container--active");
+		document.querySelector("body").style.overflowY = "auto";
 	}
 	Array.from(document.querySelectorAll(".etc-image-overlay__preview")).forEach(function(e) {
 		e.onclick = function() {
 			document.querySelector(".etc-image-overlay__container").classList.add("etc-image-overlay__container--active");
+			document.querySelector("body").style.overflowY = "hidden";
+			var scrollY = window.scrollY;
+			document.querySelector(".etc-image-overlay__container").style.top = String(scrollY) + "px";
 		}
 	})	
 })();
