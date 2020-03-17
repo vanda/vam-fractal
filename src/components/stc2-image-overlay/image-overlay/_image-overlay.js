8ef976a3e3715a5883686dbe58cf18b02dfb92f7
Patrick Cartlidge <patrickcartlidge@gmail.com>
2020-03-17 11:13:35 +0000

    fixed more y scroll issue

--- src/components/stc2-image-overlay/image-overlay/_image-overlay.js
+++ src/components/stc2-image-overlay/image-overlay/_image-overlay.js
@@ -10,7 +10,7 @@
 	function openObjectOverlay() {
 		document.querySelector(".etc-image-overlay__container").classList.add("etc-image-overlay__container--active");
 		document.querySelector("body").style.overflowY = "hidden";
-		var scrollY = window.scrollY;
+		var scrollY = window.pageYOffset;
 		document.querySelector(".etc-image-overlay__container").style.top = String(scrollY) + "px";
 	}
 
