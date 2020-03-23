97f070f11a1b3237180d9981fefb13c8f5c46eb2
Patrick Cartlidge <patrickcartlidge@gmail.com>
2020-03-23 10:33:54 +0000

    more style tweaks and added content to license modal

--- src/components/stc2-image-overlay/download-button/_download-button.js
+++ src/components/stc2-image-overlay/download-button/_download-button.js
@@ -1,31 +1,52 @@
 (function(){
-	var links = [
-		"download-button__download-icon-link",
-		"download-button__download-link-text"
+	var buttons = [
+		{
+			button: "__license-button",
+			content: "__license-content"
+		},
+		{
+			button: "__download-button",
+			content: "__download-content",
+
+		}
 	];
 
-	if (document.querySelector(".download-button__button")) {
-		document.querySelector(".download-button__button").onclick = function(e) {
-			document.querySelector(".download-button__modal").classList.add("b-modal--active");
+	var prefix = "image-button";
+
+	buttons.forEach(function(c) {
+		document.querySelector("." + prefix + c.button + " button").onclick = function() {
+			document.querySelector("." + prefix + c.content).classList.add(prefix + c.content + "--active");
+		}
+	});
+
+	if (document.querySelector(".image-button__download-button")) {
+		document.querySelector(".image-button__download-button").onclick = function(e) {
+			document.querySelector(".image-button__download-modal").classList.add("b-modal--active");
 		}
 	}
 
-	document.querySelector(".download-button__checkbox-container").onclick = function(e) {
-		if (e.target === document.querySelector(".download-button__agree-to-terms")) {
-			document.querySelector(".download-button__agree-checkbox").checked =
-				!document.querySelector(".download-button__agree-checkbox").checked;
+	if (document.querySelector(".image-button__license-button")) {
+		document.querySelector(".image-button__license-button").onclick = function(e) {
+			document.querySelector(".image-button__license-modal").classList.add("b-modal--active");
+		}
+	}	
 
-				var prefix = "download-button";
+	document.querySelector(".image-button__checkbox-container").onclick = function(e) {
+		if (e.target === document.querySelector(".image-button__agree-to-terms")) {
+			document.querySelector(".image-button__agree-checkbox").checked =
+				!document.querySelector(".image-button__agree-checkbox").checked;
 
 				var elements = [
 					"__icon-link-container",
 					"__checkmark",
-					"__download-icon-link",
-					"__agree-to-terms"
+					"__image-icon-link",
+					"__agree-to-terms",
+					"__download-content",
+					"__license-content"
 				];
 
-				if (document.querySelector(".download-button__agree-checkbox").checked) {
-					document.querySelector(".download-button__agree-to-terms-reminder").style.display = "none";
+				if (document.querySelector(".image-button__agree-checkbox").checked) {
+					document.querySelector(".image-button__agree-to-terms-reminder").style.display = "none";
 
 					elements.forEach(function(c) {
 						document.querySelector("." + prefix + c).classList.add(prefix + c + "--active");
@@ -39,12 +60,17 @@
 		}
 	}
 
+	var links = [
+		"__download-icon-link",
+		"__download-link-text"
+	];
+
 	links.forEach(function(link) {
-		if (document.querySelector("." + link)) {
-			document.querySelector("." + link).onclick = function(e) {
-				if (!document.querySelector(".download-button__agree-checkbox").checked) {
-					document.querySelector(".download-button__agree-to-terms-reminder").style.display = "block";
-					document.querySelector(".download-button__agree-to-terms").classList.add("download-button__agree-to-terms--warning");
+		if (document.querySelector("." + prefix + link)) {
+			document.querySelector("." + prefix + link).onclick = function(e) {
+				if (!document.querySelector(".image-button__agree-checkbox").checked) {
+					document.querySelector(".image-button__agree-to-terms-reminder").style.display = "block";
+					document.querySelector(".image-button__agree-to-terms").classList.add("image-button__agree-to-terms--warning");
 				}
 			}
 		}
