e4c20a360cba18b66e71ffe84719936cb9e2b315
Patrick Cartlidge <patrickcartlidge@gmail.com>
2020-04-28 12:29:21 +0100

    added event for onclose image overlay

--- src/components/stc2-image-overlay/image-overlay/_image-overlay.js
+++ src/components/stc2-image-overlay/image-overlay/_image-overlay.js
@@ -1,6 +1,7 @@
 (function(){
 	if (document.querySelector(".etc-image-overlay")) {
-		document.querySelector(".etc-image-overlay__close").onclick = function() {
+		document.querySelector(".etc-image-overlay__close").onclick = function(e) {
+			e.target.dispatchEvent(new Event("closedImageOverlay"), { bubbles: true });
 			document.querySelector(".etc-image-overlay__container").classList.remove("etc-image-overlay__container--active");
 			document.querySelector("body").style.overflowY = "auto";
 		}
