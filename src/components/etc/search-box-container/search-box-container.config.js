d8f1fc7a16ea04772f4ff36fe4ef16f40a134bc7
Patrick Cartlidge <patrickcartlidge@gmail.com>
2020-04-30 16:01:21 +0100

    added listings search box

---
+++ src/components/etc/search-box-container/search-box-container.config.js
@@ -0,0 +1,18 @@
+module.exports = {  
+  variants: [
+		{
+      		name: 'default',
+      		label: 'Default',
+      		context: {
+                        modifier: 'start'
+      		}
+	    },
+	    {
+	    	name: 'search-result-listings',
+	    	label: 'Search Result Listing Search',
+	    	context: {
+          		modifier: 'listing'
+	    	}
+	    }
+    ]
+}
