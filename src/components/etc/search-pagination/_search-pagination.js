f64c0836ce7cbbd0b1614f1b3d084ea242dcc8e9
Patrick Cartlidge <patrickcartlidge@gmail.com>
2020-05-06 12:19:13 +0100

    more style fixes to the search results page

--- src/components/etc/search-pagination/_search-pagination.js
+++ src/components/etc/search-pagination/_search-pagination.js
@@ -25,27 +25,29 @@ Array.from(document.querySelectorAll("." + button_class)).forEach(function (e) {
 	e.onclick = function(e) {
 		document.querySelector("." + current_button_class).classList.remove(current_button_class);
 		e.target.classList.add(current_button_class);
-
-		if (parseInt(e.target.getAttribute("page-index")) > 1) {
-			document.querySelector("." + search_prev_link_class).classList.remove(inactive_search_prev_link_class);
-		} else {
-			document.querySelector("." + search_prev_link_class).classList.add(inactive_search_prev_link_class);			
-		}
-
-		if (parseInt(e.target.getAttribute("page-index")) < number_of_pages) {
-			document.querySelector("." + search_next_link_class).classList.remove(inactive_search_next_link_class);
-		} else {
-			document.querySelector("." + search_next_link_class).classList.add(inactive_search_next_link_class);			
-		}
-
 		e.target.dispatchEvent(new Event("changeSearchPage", { bubbles: true }));
 	}
 });
 
 document.querySelector(".search-pagination").addEventListener("changeSearchPage", function() {
 	var currentButton = document.querySelector("." + current_button_class);
+	var currentButtonIndex = parseInt(currentButton.getAttribute("page-index"));
+	
+
+	if (currentButtonIndex > 1) {
+		document.querySelector("." + search_prev_link_class).classList.remove(inactive_search_prev_link_class);
+	} else {
+		document.querySelector("." + search_prev_link_class).classList.add(inactive_search_prev_link_class);			
+	}
+
+	if (currentButtonIndex < number_of_pages) {
+		document.querySelector("." + search_next_link_class).classList.remove(inactive_search_next_link_class);
+	} else {
+		document.querySelector("." + search_next_link_class).classList.add(inactive_search_next_link_class);			
+	}
+
+	var currentButton = document.querySelector("." + current_button_class);
 	if (number_of_pages > 4) {
-		var currentButtonIndex = parseInt(currentButton.getAttribute("page-index"));
 		if (currentButtonIndex >= 3 && (currentButtonIndex < number_of_pages - 1)) {
 
 			document.querySelector("." + seperator_class + start).classList.remove(seperator_class + start + inactive);
@@ -103,6 +105,31 @@ if (number_of_pages > 4) {
 			e.classList.add(button_class + inactive);
 		});
 	}
+}
+
+function increment_decrement_page(op) {
+	var currentButton = parseInt(document.querySelector("." + current_button_class).getAttribute("page-index"));
+
+	if ((currentButton > 1 && op === "-")  || (op === "+" && currentButton < number_of_pages))
+	{
+		document.querySelector("." + current_button_class).classList.remove(current_button_class);
+		if (op === '+') {
+			document.querySelector("button[page-index='" + String(currentButton + 1) + "']").classList.add(current_button_class);
+		} else {
+			document.querySelector("button[page-index='" + String(currentButton - 1) + "']").classList.add(current_button_class);
+		}		
+	}
+
+}
+
+document.querySelector(".search-pagination__prev-link").onclick = function(e) {
+	increment_decrement_page("-");
+	e.target.dispatchEvent(new Event("changeSearchPage", { bubbles: true }));
+}
+
+document.querySelector(".search-pagination__next-link").onclick = function(e) {
+	increment_decrement_page("+");
+	e.target.dispatchEvent(new Event("changeSearchPage", { bubbles: true }));
 }
 
 document.dispatchEvent(new Event("changeSearchPage", { bubbles: true }));
