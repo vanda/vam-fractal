f3889c127059716182432ecafe66c45ce726356c
methodog <sittingbottom@gmail.com>
2020-05-22 16:54:29 +0100

    add site-nav

---
+++ src/components/blocks/site-nav/_site-nav.js
@@ -0,0 +1,215 @@
+import cookies from 'browser-cookies';
+import smoothScroll from 'smooth-scroll';
+
+const siteNav = document.querySelector('.js-site-nav');
+
+if (siteNav) {
+  const winHeight = document.querySelector('html').scrollHeight;
+  const mobileNavToggle = document.querySelectorAll('.js-site-nav__mobile-toggle');
+  const mobileNavToggleIcon = document.querySelector('.js-site-nav__mobile-toggle--icon');
+  const mobileNavToggleText = document.querySelector('.js-site-nav__mobile-toggle-text');
+  const mobileNavTop = document.querySelector('.js-site-nav__mobile-top');
+  const navBag = document.querySelector('.js-site-nav-bag');
+  const navBagTotal = document.querySelector('.js-site-nav-bag-total');
+  const navSearchBtn = document.querySelector('.js-site-nav-search-btn');
+  const navSearch = document.querySelector('.js-site-nav-search');
+  const navSearchInput = document.querySelector('.js-search-input');
+  const navSearchContent = document.querySelector('.js-search-content');
+  const navSearchSuggest = document.querySelector('.js-search-suggest');
+  const shopCookieBagTotal = cookies.get('basketCount');
+  const tabletNavToggle = document.querySelector('.js-site-nav__tablet-toggle');
+
+  const scrollMonitor = (callback) => {
+    let lastScrollY = 0;
+    let ticking = false;
+
+    window.addEventListener('scroll', (e) => {
+      e.preventDefault();
+      lastScrollY = window.pageYOffset;
+      if (!ticking) {
+        window.requestAnimationFrame(() => {
+          callback.call(this, lastScrollY);
+          ticking = false;
+        });
+      }
+      ticking = true;
+      return true;
+    }, false);
+  };
+
+  if (!!shopCookieBagTotal && shopCookieBagTotal > 0) {
+    if (navBag) {
+      navBag.classList.remove('b-site-nav__bag--hidden');
+    }
+    navBagTotal.innerHTML = shopCookieBagTotal < 100 ? shopCookieBagTotal : '';
+    if (mobileNavToggleIcon) {
+      mobileNavToggleText.classList.add('b-site-nav__mobile-toggle-text--hidden');
+    }
+  }
+
+  if (mobileNavToggleIcon && window.getComputedStyle(mobileNavToggleIcon).display !== 'none') {
+    const scrollReact = (scrollY) => {
+      if (scrollY > 60) {
+        mobileNavToggleIcon.classList.add('b-site-nav__mobile-toggle--solo');
+      } else {
+        mobileNavToggleIcon.classList.remove('b-site-nav__mobile-toggle--solo');
+      }
+      if (scrollY / winHeight > 0.35) {
+        mobileNavTop.classList.add('b-site-nav__mobile-top--show');
+      } else {
+        mobileNavTop.classList.remove('b-site-nav__mobile-top--show');
+      }
+    };
+    scrollMonitor(scrollReact);
+
+    mobileNavTop.addEventListener('click', () => {
+      smoothScroll.animateScroll(0);
+    });
+  }
+
+  Array.from(mobileNavToggle, (toggle) => {
+    toggle.addEventListener('click', (e) => {
+      e.preventDefault();
+      siteNav.classList.toggle('b-site-nav--open');
+      toggle.classList.toggle('active');
+    }, false);
+    return true;
+  });
+
+  if (tabletNavToggle) {
+    tabletNavToggle.addEventListener('click', (e) => {
+      e.preventDefault();
+      document.querySelector('.js-site-nav-more').classList.toggle('active');
+    }, false);
+  }
+
+  let storedSearch = JSON.parse(sessionStorage.getItem('storedSearch'));
+  if (!storedSearch || Date.parse(new Date()) > Date.parse(storedSearch.expires)) {
+    const httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', 'https://vam.ac.uk/services/search/suggest/promoted');
+    httpRequest.send();
+    httpRequest.onreadystatechange = () => {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          const expires = new Date();
+          const storedSearchResults = {
+            expires: new Date(expires.setMinutes(expires.getMinutes() + 15)),
+            results: httpRequest.responseText
+          };
+          sessionStorage.setItem('storedSearch', JSON.stringify(storedSearchResults));
+          storedSearch = JSON.parse(sessionStorage.getItem('storedSearch'));
+        }
+      }
+    };
+  }
+
+  const trackAutosuggest = () => {
+    window.dataLayer.push({
+      event: 'autosuggest search',
+      eventCategory: this.tracking.eventCategory,
+      eventAction: this.tracking.eventAction,
+      eventLabel: this.tracking.eventLabel
+    });
+  };
+
+  const autoSuggest = (regEx, json, term, liveResult) => {
+    let i = null;
+    let title = null;
+    let imgSrc = null;
+    JSON.parse(json).forEach((result) => {
+      for (i = 0; i < result.suggestions.length; i += 1) {
+        title = result.suggestions[i].title;
+        if (result.suggestions[i].terms) {
+          title = result.suggestions[i].terms.join('|');
+        }
+        if (regEx.test(title)) {
+          navSearchContent.classList.remove('b-site-nav__core__search__container--hidden');
+          if (result.suggestions[i].icon) {
+            imgSrc = result.suggestions[i].icon;
+          } else {
+            imgSrc = 'https://s3-eu-west-1.amazonaws.com/vanda-production-assets/static/search/result_placeholder.jpg';
+          }
+          const suggestion = document.createElement('li');
+          if (liveResult) { suggestion.classList.add('js-live-result'); }
+          suggestion.innerHTML = `
+            <div class="b-search-result-auto-suggest">
+              <a class="b-search-result-auto-suggest-content"
+                href="${result.suggestions[i].url}">
+                <img class="b-search-result-auto-suggest-image"
+                    src="${imgSrc}"
+                    alt=""
+                    aria-hidden="true">
+                <div class="b-search-result-auto-suggest-text">
+                  <div class="b-search-result-auto-suggest-content-type">
+                    ${result.suggestions[i].type}
+                  </div>
+                  <div class="b-search-result-auto-suggest-title js-search-result-text">
+                    ${result.suggestions[i].title}
+                  </div>
+                </div>
+              </a>
+            </div>
+          `;
+          suggestion.tracking = {
+            eventCategory: `search - autosuggest - ${result.suggestions[i].type}`,
+            eventAction: term,
+            eventLabel: result.suggestions[i].url
+          };
+          suggestion.addEventListener('click', trackAutosuggest);
+          navSearchSuggest.appendChild(suggestion);
+        }
+      }
+    });
+  };
+
+  if (navSearchBtn) {
+    navSearchBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      navSearchBtn.classList.toggle('b-site-nav__core__search-btn--active');
+      navSearch.classList.toggle('b-site-nav__core__search--active');
+      navSearchInput.focus();
+      navSearchInput.setSelectionRange(100, 100);
+    }, false);
+  }
+
+  if (navSearchInput) {
+    navSearchInput.addEventListener('input', () => {
+      if (navSearchInput.value.length < 42) {
+        navSearchContent.classList.add('b-site-nav__core__search__container--hidden');
+        navSearchSuggest.innerHTML = '';
+        if (navSearchInput.value.length) {
+          /* title (or one of the pipe delimited terms for hot-topics)
+           * begins with search term */
+          let regExStart = '(^|\\|)';
+          if (navSearchInput.value.length > 2) {
+            /* any word in the title begins with search term
+             * (all hot-topic terms get tested since pipe delimiters count as word-boundaries) */
+            regExStart = '\\b';
+          }
+          const term = navSearchInput.value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+          const regEx = new RegExp(`${regExStart}${term}.*`, 'i');
+          autoSuggest(regEx, storedSearch.results, navSearchInput.value, false);
+          if (navSearchInput.value.length > 4) {
+            const httpRequest = new XMLHttpRequest();
+            httpRequest.open(
+              'GET',
+              `https:/vam.ac.uk/services/search/suggest/popular?q=${encodeURI(navSearchInput.value)}`
+            );
+            httpRequest.send();
+            httpRequest.onreadystatechange = () => {
+              if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                if (httpRequest.status === 200) {
+                  Array.from(navSearchSuggest.querySelectorAll('.js-live-result'), (el) => {
+                    el.remove();
+                    return true;
+                  });
+                  autoSuggest(regEx, httpRequest.responseText, navSearchInput.value, true);
+                }
+              }
+            };
+          }
+        }
+      }
+    }, false);
+  }
+}
