ca3b8bb3a94aa1e68d395a40f68ccd0d0aedc92e
Patrick Cartlidge <patrickcartlidge@gmail.com>
2020-05-28 14:16:00 +0100

    rewrote the pagination because it was bad and i dont know why i wrote it like that

--- src/components/blocks/search-pagination/_search-pagination.js
+++ src/components/blocks/search-pagination/_search-pagination.js
@@ -7,19 +7,20 @@ var start = "-start";
 var middle = "-middle";
 var last = "-last";
 var inactive = "--inactive";
+var active = "--active";
 var current = "--current";
 var current_button_class = button_class + current;
 var inactive_search_prev_link_class = search_prev_link_class + inactive;
 var inactive_search_next_link_class = search_next_link_class + inactive;
-var number_of_pages = document.querySelectorAll("." + button_class).length;
+var number_of_pages = document.querySelectorAll("." + button_class).length + 2;
 
 
 function hideButton(index) {
-	document.querySelector("button[page-index='" + String(index) + "']").classList.add(button_class + inactive);
+	document.querySelector("button[page-index='" + String(index) + "']").classList.remove(button_class + active);
 }
 
 function showButton(index) {
-	document.querySelector("button[page-index='" + String(index) + "']").classList.remove(button_class + inactive);	
+	document.querySelector("button[page-index='" + String(index) + "']").classList.add(button_class + active);	
 }
 
 Array.from(document.querySelectorAll("." + button_class)).forEach(function (e) {
@@ -30,78 +31,73 @@ Array.from(document.querySelectorAll("." + button_class)).forEach(function (e) {
 	}
 });
 
+document.querySelector("." + button_class + start).onclick = function(e) {
+	document.querySelector("." + current_button_class).classList.remove(current_button_class);
+	e.target.classList.add(current_button_class);
+	e.target.dispatchEvent(new Event("changeSearchPage", { bubbles: true }));
+}
+
+document.querySelector("." + button_class + last).onclick = function(e) {
+	document.querySelector("." + current_button_class).classList.remove(current_button_class);
+	e.target.classList.add(current_button_class);
+	e.target.dispatchEvent(new Event("changeSearchPage", { bubbles: true }));
+}
+
 document.addEventListener('DOMContentLoaded', function (event) {
 	document.querySelector(".b-search-pagination").addEventListener("changeSearchPage", function() {
-		var currentButton = document.querySelector("." + current_button_class);
-		var currentButtonIndex = parseInt(currentButton.getAttribute("page-index"));
+		var current_button = document.querySelector("." + current_button_class);
+		var current_button_index = parseInt(current_button.getAttribute("page-index"));
 		
-		if (currentButtonIndex > 1) {
+		if (current_button_index > 1) {
 			document.querySelector("." + search_prev_link_class).classList.remove(inactive_search_prev_link_class);
 		} else {
 			document.querySelector("." + search_prev_link_class).classList.add(inactive_search_prev_link_class);			
 		}
 
-		if (currentButtonIndex < number_of_pages) {
+		if (current_button_index < number_of_pages) {
 			document.querySelector("." + search_next_link_class).classList.remove(inactive_search_next_link_class);
 		} else {
 			document.querySelector("." + search_next_link_class).classList.add(inactive_search_next_link_class);			
 		}
 
-		var currentButton = document.querySelector("." + current_button_class);
+		var current_button = document.querySelector("." + current_button_class);
 		if (number_of_pages > 4) {
-			if (currentButtonIndex >= 3 && (currentButtonIndex < number_of_pages - 1)) {
+			// activate this logic...
+			document.querySelectorAll("." + button_class + active).forEach(function(e) {
+				e.classList.remove(button_class + active)
+			});
 
-				document.querySelector("." + seperator_class + start).classList.remove(seperator_class + start + inactive);
+			if ((number_of_pages - current_button_index >= 3 && current_button_index > 2 && current_button_index < number_of_pages - 1) || current_button_index == number_of_pages - 2) {
+				document.querySelector("." + seperator_class + start).classList.remove(seperator_class + start + inactive);				
 				document.querySelector("." + seperator_class + middle).classList.add(seperator_class + middle + inactive);
 				document.querySelector("." + seperator_class + last).classList.remove(seperator_class + last + inactive);
 
-				for (var i = currentButtonIndex - 1; i < currentButtonIndex + 2; i++) {
-					showButton(i);
-				}
+				showButton(current_button_index - 1);
+				showButton(current_button_index);
+				showButton(current_button_index + 1);
 
-				hideButton(currentButtonIndex - 2);
-				hideButton(currentButtonIndex + 2);
-				
-				showButton(1);
-				showButton(number_of_pages);
-			} else if (currentButtonIndex <= 2) {
-				document.querySelector("." + seperator_class + start).classList.add(seperator_class + start + inactive);
-				document.querySelector("." + seperator_class + middle).classList.remove(seperator_class + middle + inactive);
-				document.querySelector("." + seperator_class + last).classList.add(seperator_class + last + inactive);			
-
-				for (var i = 1; i < 3; i++) {
-					showButton(i);
-				}
-
-				Array.from(document.querySelectorAll("." + button_class)).slice(3, number_of_pages - 1).forEach(function(e) {
-					e.classList.add(button_class + inactive);
-				});
-
-				if (currentButtonIndex === 1) {
+			} else {
+				if (current_button_index <= 2) {
+					showButton(2);
 					showButton(3);
+					document.querySelector("." + seperator_class + start).classList.add(seperator_class + start + inactive);
+					document.querySelector("." + seperator_class + middle).classList.add(seperator_class + middle + inactive);
+					document.querySelector("." + seperator_class + last).classList.remove(seperator_class + last + inactive);
+				} else if (current_button_index >= number_of_pages - 1 && current_button_index <= number_of_pages) {
+					showButton(number_of_pages - 1);
+					showButton(number_of_pages - 2);
+					document.querySelector("." + seperator_class + start).classList.remove(seperator_class + start + inactive);
+					document.querySelector("." + seperator_class + middle).classList.add(seperator_class + middle + inactive);
+					document.querySelector("." + seperator_class + last).classList.add(seperator_class + last + inactive);
 				}
-			} else if (currentButtonIndex > 3 && (currentButtonIndex >= number_of_pages - 1)) {
-				document.querySelector("." + seperator_class + start).classList.remove(seperator_class + start + inactive);
-				document.querySelector("." + seperator_class + middle).classList.add(seperator_class + middle + inactive);
-				document.querySelector("." + seperator_class + last).classList.add(seperator_class + last + inactive);
-
-				Array.from(document.querySelectorAll("." + button_class)).slice(1, number_of_pages - 3).forEach(function(e) {
-					e.classList.add(button_class + inactive);
-				});
-
-				if (currentButtonIndex === number_of_pages) {
-					for (var i = number_of_pages - 2; i < number_of_pages; i++) {
-						showButton(i)
-					}				
-				}			
 			}
 		}
 	}, true);
 
 	if (number_of_pages > 4) {
 		document.querySelector("." + seperator_class + middle).classList.remove(seperator_class + middle + inactive)
-		var currentButtonIndex = parseInt(document.querySelector("." + current_button_class).getAttribute("page-index"));
-		if (currentButtonIndex < 3) {
+		var current_button_index = parseInt(document.querySelector("." + current_button_class).getAttribute("page-index"));
+		if (current_button_index < 3) {
 			Array.from(document.querySelectorAll("." + button_class)).slice(3, number_of_pages - 1).forEach(function(e) {
 				e.classList.add(button_class + inactive);
 			});
@@ -109,15 +105,15 @@ document.addEventListener('DOMContentLoaded', function (event) {
 	}
 
 	function increment_decrement_page(op) {
-		var currentButton = parseInt(document.querySelector("." + current_button_class).getAttribute("page-index"));
+		var current_button = parseInt(document.querySelector("." + current_button_class).getAttribute("page-index"));
 
-		if ((currentButton > 1 && op === "-")  || (op === "+" && currentButton < number_of_pages))
+		if ((current_button > 1 && op === "-")  || (op === "+" && current_button < number_of_pages))
 		{
 			document.querySelector("." + current_button_class).classList.remove(current_button_class);
 			if (op === '+') {
-				document.querySelector("button[page-index='" + String(currentButton + 1) + "']").classList.add(current_button_class);
+				document.querySelector("button[page-index='" + String(current_button + 1) + "']").classList.add(current_button_class);
 			} else {
-				document.querySelector("button[page-index='" + String(currentButton - 1) + "']").classList.add(current_button_class);
+				document.querySelector("button[page-index='" + String(current_button - 1) + "']").classList.add(current_button_class);
 			}		
 		}
 
@@ -134,4 +130,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
 	}
 
 	document.dispatchEvent(new Event("changeSearchPage", { bubbles: true }));
+
+	var current_button = parseInt(document.querySelector("." + current_button_class).getAttribute("page-index"));
+	document.querySelector("button[page-index='" + String(current_button) + "']").click();
 })
