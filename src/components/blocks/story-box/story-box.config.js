56148c4393207fed09625fc3f920de28a60c6752
Patrick Cartlidge <patrickcartlidge@gmail.com>
2020-07-17 16:28:51 +0100

    reimplemented to support col and row styles

---
+++ src/components/blocks/story-box/story-box.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  variants: [
+    {
+      name: 'default',
+      label: 'Default',
+      context: {
+        modifier: "column"
+      }
+    },
+    {
+      name: 'row',
+      label: 'row',
+      context: {
+        modifier: "row"
+      }
+    }
+  ]
+};
