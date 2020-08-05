f70d8b73e3005e94710b606ad7ae25fe2a26f8d2
Luca Carini <l.carini@vam.ac.uk>
2020-08-05 19:25:13 +0100

    STC2-267 new etc search box (#61)
    * modify search form for EtC
    
    * linting :(

--- src/components/blocks/search-form/search-form.config.js
+++ src/components/blocks/search-form/search-form.config.js
@@ -1,7 +1,20 @@
 module.exports = {
-  title: 'Search Form',
+  title: 'Site Search Form',
+  name: 'Search Form',
   label: 'Search Form',
   context: {
+    inputId: 'q',
     placeholder: 'Type your search here'
-  }
+  },
+  variants: [
+    {
+      name: 'Collections Search',
+      label: 'Collections Search',
+      context: {
+        modifiers: ['etc'],
+        inputId: 'q-etc',
+        placeholder: 'Search by object, artist, maker&hellip;'
+      }
+    }
+  ]
 };
--- src/components/blocks/search-pagination/_search-pagination.js
+++ src/components/blocks/search-pagination/_search-pagination.js
@@ -46,97 +46,99 @@ if (document.querySelector(`.${buttonClass}${last}`)) {
   };
 }
 
-document.querySelector('.b-search-pagination') && document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('.b-search-pagination').addEventListener('changeSearchPage', () => {
-    const currentButton = document.querySelector(`.${currentButtonClass}`);
-    const currentButtonIndex = parseInt(currentButton.getAttribute('page-index'), 10);
+document.addEventListener('DOMContentLoaded', () => {
+  if (document.querySelector(searchPaginationContainer)) {
+    document.querySelector(searchPaginationContainer).addEventListener('changeSearchPage', () => {
+      const currentButton = document.querySelector(`.${currentButtonClass}`);
+      const currentButtonIndex = parseInt(currentButton.getAttribute('page-index'), 10);
 
-    if (currentButtonIndex > 1) {
-      document.querySelector(`.${searchPrevLinkClass}`).classList.remove(inactiveSearchPrevLinkClass);
-    } else {
-      document.querySelector(`.${searchPrevLinkClass}`).classList.add(inactiveSearchPrevLinkClass);
-    }
-
-    if (currentButtonIndex < numberOfPages) {
-      document.querySelector(`.${searchNextLinkClass}`).classList.remove(inactiveSearchNextLinkClass);
-    } else {
-      document.querySelector(`.${searchNextLinkClass}`).classList.add(inactiveSearchNextLinkClass);
-    }
-
-    if (numberOfPages > 4) {
-      // activate this logic...
-      document.querySelectorAll(`.${buttonClass}${active}`).forEach((e, i) => {
-        hideButton(i);
-      });
-
-      if (
-        (
-          numberOfPages - currentButtonIndex >= 3 &&
-          currentButtonIndex > 2 &&
-          currentButtonIndex < numberOfPages - 1
-        ) || currentButtonIndex === numberOfPages - 2
-      ) {
-        document.querySelector(`.${seperatorClass}${start}`).classList.remove(seperatorClass + start + inactive);
-        document.querySelector(`.${seperatorClass}${middle}`).classList.add(seperatorClass + middle + inactive);
-        document.querySelector(`.${seperatorClass}${last}`).classList.remove(seperatorClass + last + inactive);
-
-        showButton(currentButtonIndex - 1);
-        showButton(currentButtonIndex);
-        showButton(currentButtonIndex + 1);
+      if (currentButtonIndex > 1) {
+        document.querySelector(`.${searchPrevLinkClass}`).classList.remove(inactiveSearchPrevLinkClass);
       } else {
-        if (currentButtonIndex <= 2) {
-          showButton(2);
-          showButton(3);
-          document.querySelector(`.${seperatorClass}${start}`).classList.add(seperatorClass + start + inactive);
-          document.querySelector(`.${seperatorClass}${middle}`).classList.add(seperatorClass + middle + inactive);
-          document.querySelector(`.${seperatorClass}${last}`).classList.remove(seperatorClass + last + inactive);
-        }
-        if (currentButtonIndex >= numberOfPages - 1 && currentButtonIndex <= numberOfPages) {
-          showButton(numberOfPages - 1);
-          showButton(numberOfPages - 2);
+        document.querySelector(`.${searchPrevLinkClass}`).classList.add(inactiveSearchPrevLinkClass);
+      }
+
+      if (currentButtonIndex < numberOfPages) {
+        document.querySelector(`.${searchNextLinkClass}`).classList.remove(inactiveSearchNextLinkClass);
+      } else {
+        document.querySelector(`.${searchNextLinkClass}`).classList.add(inactiveSearchNextLinkClass);
+      }
+
+      if (numberOfPages > 4) {
+        // activate this logic...
+        document.querySelectorAll(`.${buttonClass}${active}`).forEach((e, i) => {
+          hideButton(i);
+        });
+
+        if (
+          (
+            numberOfPages - currentButtonIndex >= 3 &&
+            currentButtonIndex > 2 &&
+            currentButtonIndex < numberOfPages - 1
+          ) || currentButtonIndex === numberOfPages - 2
+        ) {
           document.querySelector(`.${seperatorClass}${start}`).classList.remove(seperatorClass + start + inactive);
           document.querySelector(`.${seperatorClass}${middle}`).classList.add(seperatorClass + middle + inactive);
-          document.querySelector(`.${seperatorClass}${last}`).classList.add(seperatorClass + last + inactive);
+          document.querySelector(`.${seperatorClass}${last}`).classList.remove(seperatorClass + last + inactive);
+
+          showButton(currentButtonIndex - 1);
+          showButton(currentButtonIndex);
+          showButton(currentButtonIndex + 1);
+        } else {
+          if (currentButtonIndex <= 2) {
+            showButton(2);
+            showButton(3);
+            document.querySelector(`.${seperatorClass}${start}`).classList.add(seperatorClass + start + inactive);
+            document.querySelector(`.${seperatorClass}${middle}`).classList.add(seperatorClass + middle + inactive);
+            document.querySelector(`.${seperatorClass}${last}`).classList.remove(seperatorClass + last + inactive);
+          }
+          if (currentButtonIndex >= numberOfPages - 1 && currentButtonIndex <= numberOfPages) {
+            showButton(numberOfPages - 1);
+            showButton(numberOfPages - 2);
+            document.querySelector(`.${seperatorClass}${start}`).classList.remove(seperatorClass + start + inactive);
+            document.querySelector(`.${seperatorClass}${middle}`).classList.add(seperatorClass + middle + inactive);
+            document.querySelector(`.${seperatorClass}${last}`).classList.add(seperatorClass + last + inactive);
+          }
         }
       }
-    }
-  }, true);
+    }, true);
 
-  if (numberOfPages > 4) {
-    document.querySelector(`.${seperatorClass}${middle}`).classList.remove(seperatorClass + middle + inactive);
-    const currentButtonIndex = parseInt(document.querySelector(`.${currentButtonClass}`).getAttribute('page-index'), 10);
-    if (currentButtonIndex < 3) {
-      Array.from(document.querySelectorAll(`.${buttonClass}`)).slice(3, numberOfPages - 1).forEach((e) => {
-        e.classList.add(buttonClass + inactive);
-      });
-    }
-  }
-
-  function incrementDecrementPage (op) {
-    const currentButton = parseInt(document.querySelector(`.${currentButtonClass}`).getAttribute('page-index'), 10);
-
-    if ((currentButton > 1 && op === '-') || (op === '+' && currentButton < numberOfPages)) {
-      document.querySelector(`.${currentButtonClass}`).classList.remove(currentButtonClass);
-      if (op === '+') {
-        document.querySelector(`button[page-index='${currentButton + 1}']`).classList.add(currentButtonClass);
-      } else {
-        document.querySelector(`button[page-index='${currentButton - 1}']`).classList.add(currentButtonClass);
+    if (numberOfPages > 4) {
+      document.querySelector(`.${seperatorClass}${middle}`).classList.remove(seperatorClass + middle + inactive);
+      const currentButtonIndex = parseInt(document.querySelector(`.${currentButtonClass}`).getAttribute('page-index'), 10);
+      if (currentButtonIndex < 3) {
+        Array.from(document.querySelectorAll(`.${buttonClass}`)).slice(3, numberOfPages - 1).forEach((e) => {
+          e.classList.add(buttonClass + inactive);
+        });
       }
     }
+
+    const incrementDecrementPage = (op) => {
+      const currentButton = parseInt(document.querySelector(`.${currentButtonClass}`).getAttribute('page-index'), 10);
+
+      if ((currentButton > 1 && op === '-') || (op === '+' && currentButton < numberOfPages)) {
+        document.querySelector(`.${currentButtonClass}`).classList.remove(currentButtonClass);
+        if (op === '+') {
+          document.querySelector(`button[page-index='${currentButton + 1}']`).classList.add(currentButtonClass);
+        } else {
+          document.querySelector(`button[page-index='${currentButton - 1}']`).classList.add(currentButtonClass);
+        }
+      }
+    };
+
+    document.querySelector(`.${searchPrevLinkClass}`).onclick = (e) => {
+      incrementDecrementPage('-');
+      e.target.dispatchEvent(new Event('changeSearchPage', { bubbles: true }));
+    };
+
+    document.querySelector(`.${searchNextLinkClass}`).onclick = (e) => {
+      incrementDecrementPage('+');
+      e.target.dispatchEvent(new Event('changeSearchPage', { bubbles: true }));
+    };
+
+    document.dispatchEvent(new Event('changeSearchPage', { bubbles: true }));
+
+    const currentButton = parseInt(document.querySelector(`.${currentButtonClass}`).getAttribute('page-index'), 10);
+    document.querySelector(`button[page-index='${currentButton}']`).click();
   }
-
-  document.querySelector(`.${searchPrevLinkClass}`).onclick = (e) => {
-    incrementDecrementPage('-');
-    e.target.dispatchEvent(new Event('changeSearchPage', { bubbles: true }));
-  };
-
-  document.querySelector(`.${searchNextLinkClass}`).onclick = (e) => {
-    incrementDecrementPage('+');
-    e.target.dispatchEvent(new Event('changeSearchPage', { bubbles: true }));
-  };
-
-  document.dispatchEvent(new Event('changeSearchPage', { bubbles: true }));
-
-  const currentButton = parseInt(document.querySelector(`.${currentButtonClass}`).getAttribute('page-index'), 10);
-  document.querySelector(`button[page-index='${currentButton}']`).click();
 });
