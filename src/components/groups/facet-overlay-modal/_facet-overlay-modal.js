e3c126c46b3f17a5ddec3a67c4ab7e2f1345b241
Patrick Cartlidge <patrickcartlidge@gmail.com>
2020-08-07 10:44:15 +0100

    linting

--- src/components/groups/facet-overlay-modal/_facet-overlay-modal.js
+++ src/components/groups/facet-overlay-modal/_facet-overlay-modal.js
@@ -1,13 +1,12 @@
 const initFacetOverlayModal = () => {
-  document.querySelector(".facet-overlay-modal__button").onclick = e => {
-    document.querySelector(".facet-overlay-modal__container").classList.toggle(`facet-overlay-modal__container--active`);
+  document.querySelector('.facet-overlay-modal__button').onclick = (e) => {
+    document.querySelector('.facet-overlay-modal__container').classList.toggle('facet-overlay-modal__container--active');
     e.preventDefault();
-  }
+  };
 
-  document.querySelector(".facet-overlay-modal__container").addEventListener("closeFacetOverlay", e => {
-    console.log('asjkasdedhjkasdjnkasjknadsjkn')
-    document.querySelector(".facet-overlay-modal__container").classList.toggle(`facet-overlay-modal__container--active`);
-  });
-}
+  document.querySelector('.facet-overlay-modal__container').addEventListener('closeFacetOverlay', () =>
+    document.querySelector('.facet-overlay-modal__container').classList.toggle('facet-overlay-modal__container--active')
+  );
+};
 
 initFacetOverlayModal();
