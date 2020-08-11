f817dede74931a3fca78fc896adcb21855b3e0b4
methodog <sittingbottom@gmail.com>
2020-08-11 15:10:25 +0100

    add EtC search boxes with autosuggest & filters

--- src/components/blocks/search-form/_search-form.js
+++ src/components/blocks/search-form/_search-form.js
@@ -1,41 +1,174 @@
-// To draw the coloured underline beneath the input field value
-
-const searchInput = document.querySelector('.js-search-input');
-const searchUnderscore = document.querySelector('.js-search-underscore');
-const searchSubmit = document.querySelector('.js-search-submit');
-const searchClear = document.querySelector('.js-search-clear');
-
-function searchDecorate () {
-  searchUnderscore.innerHTML = searchInput.value.replace(/\s/g, '&nbsp;');
-}
-
-function searchInit () {
-  searchSubmit.classList.add('b-search-form__submit--hidden');
-  if (searchInput.value.length) {
-    searchClear.classList.remove('b-search-form__clear--hidden');
+/* eslint-disable no-underscore-dangle */
+const loadSuggestions = (searchForm) => {
+  searchForm._props.storedSuggestions = JSON.parse(sessionStorage.getItem(`storedSuggestions_${searchForm._props.type}`));
+  const now = new Date();
+  if (!searchForm._props.storedSuggestions
+    || now.getTime() > searchForm._props.storedSuggestions.expires) {
+    const promise = fetch(searchForm._props.suggestionsTop, { cache: 'no-cache' })
+    .then(response => response.json())
+    .then((data) => {
+      const suggestions = {
+        expires: now.getTime() + (15 * 60000),
+        data
+      };
+      searchForm._props.storedSuggestions = suggestions;
+      sessionStorage.setItem(`storedSuggestions_${searchForm._props.type}`, JSON.stringify(searchForm._props.storedSuggestions));
+    })
+    .catch(e => console.error(e.name, e.message)); // eslint-disable-line no-console
+    return promise;
   }
-}
+  return Promise.resolve(true);
+};
 
-function searchActivate () {
-  searchClear.classList.add('b-search-form__clear--hidden');
-  if (searchInput.value.length) {
-    searchSubmit.classList.remove('b-search-form__submit--hidden');
-  } else {
-    searchSubmit.classList.add('b-search-form__submit--hidden');
+const trackAutosuggest = (e) => {
+  window.dataLayer.push({
+    event: e.target.tracking.event,
+    eventCategory: e.target.tracking.eventCategory,
+    eventAction: e.target.tracking.eventAction,
+    eventLabel: e.target.tracking.eventLabel
+  });
+};
+
+Array.from(document.querySelectorAll('.js-search-site, .js-search-etc-gateway'), (searchForm) => {
+  const searchInput = searchForm.querySelector('.b-search-form__input');
+
+  if (searchForm.classList.contains('js-search-site')) {
+    /* Main site search */
+    searchForm._props = {
+      type: 'siteSearch'
+    };
+    const searchUnderscore = searchForm.querySelector('.b-search-form__underscore');
+    const searchSubmit = searchForm.querySelector('.b-search-form__submit');
+    const searchClear = searchForm.querySelector('.b-search-form__clear');
+
+
+    const searchDecorate = () => {
+      /* underline input field value */
+      searchUnderscore.innerHTML = searchInput.value.replace(/\s/g, '&nbsp;');
+    };
+
+    const searchActivate = () => {
+      searchClear.classList.add('b-search-form__clear--hidden');
+      if (searchInput.value.length) {
+        searchSubmit.classList.remove('b-search-form__submit--hidden');
+      } else {
+        searchSubmit.classList.add('b-search-form__submit--hidden');
+      }
+    };
+
+    const searchReset = () => {
+      searchClear.classList.add('b-search-form__clear--hidden');
+      searchInput.value = '';
+      searchDecorate();
+    };
+
+    if (searchInput.value.length) {
+      searchClear.classList.remove('b-search-form__clear--hidden');
+    } else {
+      searchSubmit.classList.add('b-search-form__submit--hidden');
+    }
+    searchInput.focus();
+    searchDecorate();
+    searchInput.addEventListener('input', () => {
+      searchDecorate();
+      searchActivate();
+    }, false);
+    searchClear.addEventListener('click', searchReset, false);
+  } else if (searchForm.classList.contains('js-search-etc-gateway')) {
+    /* EtC landing pg search */
+    searchForm._props = {
+      type: 'etcGatewaySearch',
+      suggestionsTop: '//localhost/vam-fractal/src/assets/etc-search-suggestions-top',
+      suggestionsAPI: 'http://51.11.136.250/api/v2/sayt/search'
+    };
+
+    const suggestionsEl = searchForm.querySelector('.b-search-form__suggestions');
+
+    const autoSuggest = (term, suggestion) => {
+      const suggestEl = document.createElement('a');
+      if (suggestionsEl.childElementCount < 10) {
+        const url = suggestion.uniqueID;
+        const title = suggestion.displayName || suggestion.displayTerm;
+        suggestEl.className = 'b-search-form__suggestion';
+        suggestEl.href = url;
+        suggestEl.tabindex = 0;
+        suggestEl.role = 'option';
+        suggestEl.innerHTML = `
+          <div class="b-search-form__suggestion-type">
+            ${suggestion.index}
+          </div>
+          ${title}
+        `;
+        suggestEl.tracking = {
+          event: 'autosuggest EtC landing',
+          eventCategory: `search - autosuggest - ${suggestion.index}`,
+          eventAction: term,
+          eventLabel: url
+        };
+        suggestEl.addEventListener('click', trackAutosuggest);
+        suggestionsEl.appendChild(suggestEl);
+      }
+    };
+
+    loadSuggestions(searchForm)
+    .then(() => {
+      let aborter = null;
+      searchInput.addEventListener('input', () => {
+        searchForm.removeAttribute('suggesting');
+        suggestionsEl.innerHTML = '';
+        if (searchInput.value.length) {
+          const term = searchInput.value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+          /* title (or one of the pipe delimited terms for hot-topics)
+           * begins with search term */
+          let regExStart = '(^|\\|)';
+          if (searchInput.value.length > 2) {
+            /* any word in the title begins with search term
+             * (all hot-topic terms get tested since pipe delimiters count as word-boundaries) */
+            regExStart = '\\b';
+          }
+          const regEx = new RegExp(`${regExStart}${term}.*`, 'i');
+          Array.from(searchForm._props.storedSuggestions.data, (result) => {
+            Array.from(result.suggestions, (suggestion) => {
+              const name = suggestion.displayName;
+              if (regEx.test(name)) {
+                searchForm.setAttribute('suggesting', true);
+                autoSuggest(searchInput.value, suggestion);
+              }
+              return true;
+            });
+            return true;
+          });
+          if (searchInput.value.length > 4) {
+            // cancel pending request if any
+            if (aborter) aborter.abort();
+            // make our request cancellable
+            aborter = new window.AbortController();
+            setTimeout(() => aborter.abort(), 500);
+            fetch(`${searchForm._props.suggestionsAPI}?q=${encodeURI(term)}&limit=10`, { signal: aborter.signal })
+            .then(response => response.json())
+            .then((data) => {
+              if (data.info.totalrecords > 0) {
+                searchForm.setAttribute('suggesting', true);
+                Array.from(data.records, (suggestion) => {
+                  autoSuggest(searchInput.value, suggestion);
+                  return true;
+                });
+              }
+            })
+            .catch(e => console.error(e.name, e.message)); // eslint-disable-line no-console
+          }
+        }
+      }, false);
+    })
+    .catch(e => console.error(e.name, e.message)); // eslint-disable-line no-console
+
+    Array.from(searchForm.querySelectorAll('.b-search-form__filter-toggle'), (toggle) => {
+      toggle.addEventListener('click', () => {
+        toggle.closest('.b-search-form__filter-toggle-set').toggleAttribute('active');
+      }, false);
+      return true;
+    });
   }
-}
-
-function searchReset () {
-  searchClear.classList.add('b-search-form__clear--hidden');
-  searchInput.value = '';
-  searchDecorate();
-}
-
-if (searchInput) {
-  searchInput.focus();
-  searchInit();
-  searchDecorate();
-  searchInput.addEventListener('input', searchDecorate);
-  searchInput.addEventListener('input', searchActivate);
-  searchClear.addEventListener('click', searchReset);
-}
+  return true;
+});
+/* eslint-enable no-underscore-dangle */
--- src/components/blocks/search-form/search-form.config.js
+++ src/components/blocks/search-form/search-form.config.js
@@ -3,8 +3,10 @@ module.exports = {
   name: 'Search Form',
   label: 'Search Form',
   context: {
+    jsHook: 'js-search-site',
     inputId: 'q',
-    placeholder: 'Type your search here'
+    placeholder: 'Type your search here',
+    action: 'https://www.vam.ac.uk/search'
   },
   variants: [
     {
@@ -12,8 +14,21 @@ module.exports = {
       label: 'Collections Search',
       context: {
         modifiers: ['etc'],
+        jsHook: 'js-search-etc',
         inputId: 'q-etc',
-        placeholder: 'Search by object, artist, maker&hellip;'
+        placeholder: 'Search by object, artist, maker&hellip;',
+        action: 'http://51.11.136.250/api/v2/objects/search'
+      }
+    },
+    {
+      name: 'Collections Landing',
+      label: 'Collections Landing',
+      context: {
+        modifiers: ['etc', 'etc-gateway'],
+        jsHook: 'js-search-etc-gateway',
+        inputId: 'q-etc',
+        placeholder: 'Search by object, artist, maker&hellip;',
+        action: 'http://51.11.136.250/api/v2/objects/search'
       }
     }
   ]
