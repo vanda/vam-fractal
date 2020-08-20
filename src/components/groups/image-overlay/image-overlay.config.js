4f3b0bc4e182e777434a16a63e4587c21f860861
Patrick Cartlidge <patrickcartlidge@gmail.com>
2020-08-20 15:35:03 +0100

    add cultural sensitivity filter

---
+++ src/components/groups/image-overlay/image-overlay.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: 'Image Overlay',
+  label: 'Image Overlay',
+  variants: [
+    {
+      name: 'default',
+      label: 'Default',
+      context: {}
+    },
+    {
+      name: 'Cultural Sensitive Image',
+      label: 'Cultural Sensitive Image',
+      context: {
+        offensive_image: true
+      }
+    }
+  ]
+}
