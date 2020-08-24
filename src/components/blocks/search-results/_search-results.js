4beaa8107d10e8bd3f55728055de79204a2efd31
Patrick Cartlidge <patrickcartlidge@gmail.com>
2020-08-24 13:56:23 +0100

    concealer implemented for offensive rows

--- src/components/blocks/search-results/_search-results.js
+++ src/components/blocks/search-results/_search-results.js
@@ -1,22 +1,34 @@
-const warningHTML = rowIndex => `
-  <div class="b-search-results__offensive-warning" data-row-index="${rowIndex}">
+const warningHTML = `
     <div class="b-search-results__offensive-warning-text">
       This record uses a term that has since fallen from usage and is now considered offensive. The term is repeated in this record in its original historical context.
     </div>
-    </br>
     <button class="b-search-results__offensive-warning-button">
       View record
     </button>
-  </div>
 `;
 
 const offensiveWarningInitializer = () => {
-  Array.from(document.querySelectorAll('.b-search-results__body-row--offensive')).forEach((el, i) => {
-    const warningEl = document.createElement('DIV');
-    warningEl.className = "b-search-results__offensive-warning";
-    warningEl.innerHTML = warningHTML(i);
-    warningEl.style.top = `${el.offsetTop}px`;
-    document.querySelector('.b-search-results').appendChild(warningEl);
+  Array.from(document.querySelectorAll('.b-search-results__body-row')).forEach((el, i) => {
+    if (el.classList.contains('b-search-results__body-row--offensive')) {
+      const warningEl = document.createElement('DIV');
+      warningEl.className = "b-search-results__offensive-warning";
+      warningEl.innerHTML = warningHTML;
+      warningEl.style.top = `${el.offsetTop + (el.offsetHeight / 6) }px`;
+      warningEl.style.left = `${el.getBoundingClientRect().left}px`;
+      warningEl.setAttribute('data-row-index', i);
+      warningEl.onclick = e => {
+        Array.from(document.querySelectorAll('.b-search-results__body-row'))[e.target.parentElement.getAttribute('data-row-index')].classList.remove('b-search-results__body-row--offensive');
+        e.target.parentElement.remove();
+      }
+      document.querySelector('.b-search-results').appendChild(warningEl);
+    }
+  });
+  window.addEventListener('resize', e => {
+    Array.from(document.querySelectorAll('.b-search-results__offensive-warning')).forEach(el => {
+       const row = Array.from(document.querySelectorAll('.b-search-results__body-row'))[el.getAttribute('data-row-index')];
+       el.style.top = `${row.offsetTop + + (row.offsetHeight / 6) }px`;
+       el.style.left = `${row.getBoundingClientRect().left}px`;
+    })
   });
 };
 
