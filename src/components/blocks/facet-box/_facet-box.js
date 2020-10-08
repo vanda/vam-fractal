88529c8b7d3a76c4bf3a0d0421f4de29bbc11e47
Patrick Cartlidge <patrickcartlidge@gmail.com>
2020-10-08 13:22:48 +0100

    facet box degbuggin

--- src/components/blocks/facet-box/_facet-box.js
+++ src/components/blocks/facet-box/_facet-box.js
@@ -197,6 +197,8 @@ const initialiseFacetOverlay = () => {
           document.querySelector('.b-search-form__facets').appendChild(newFormTerm);
         }
 
+        console.log(document.querySelector('.b-search-form__facet-pane--active'))
+
         if (!document.querySelector('.b-search-form__facet-pane--active')) {
           document.querySelector('.b-search-form__facet-pane').classList.add('b-search-form__facet-pane--active');
         }
