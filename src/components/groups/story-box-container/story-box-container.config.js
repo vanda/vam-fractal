39c01827819240648b678bfedd4de88c1a0f42a9
Patrick Cartlidge <patrickcartlidge@gmail.com>
2020-10-27 13:27:55 +0000

    made sure each row of story boxes have the same height

--- src/components/groups/story-box-container/story-box-container.config.js
+++ src/components/groups/story-box-container/story-box-container.config.js
@@ -2,7 +2,7 @@ const defaultStory = {
   title: 'Title Of Story',
   type: 'Content Type',
   img: 'https://vanda-production-assets.s3.amazonaws.com/2017/05/24/10/34/05/7f38c9ec-ec6a-49d0-8c0e-0eb218cb9d27/1280.jpg',
-  content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,'
+  content: 'Lorem ipsum dolor sit amet, enatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,'
 };
 
 module.exports = {
@@ -11,7 +11,27 @@ module.exports = {
       name: 'default',
       label: 'Default',
       context: {
-        stories: new Array(3).fill(defaultStory)
+        stories: [
+        {
+          title: 'Title Of Story',
+          type: 'Content Type',
+          img: 'https://vanda-production-assets.s3.amazonaws.com/2017/05/24/10/34/05/7f38c9ec-ec6a-49d0-8c0e-0eb218cb9d27/1280.jpg',
+          content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,'
+        },
+        {
+          title: 'Title Of Story',
+          type: 'Content Type',
+          img: 'https://vanda-production-assets.s3.amazonaws.com/2017/05/24/10/34/05/7f38c9ec-ec6a-49d0-8c0e-0eb218cb9d27/1280.jpg',
+          content: 'Lorem ipneanquis,'
+        },
+        {
+          title: 'Title Of Story',
+          type: 'Content Type',
+          img: 'https://vanda-production-assets.s3.amazonaws.com/2017/05/24/10/34/05/7f38c9ec-ec6a-49d0-8c0e-0eb218cb9d27/1280.jpg',
+          content: 'Lorem ipneanquis,'
+        },
+        ...(new Array(4).fill(defaultStory)),
+        ]
       }
     },
     {
