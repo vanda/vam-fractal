949851598fe5ed9496fd35711a5a389b8361bb53
Patrick Cartlidge <patrickcartlidge@gmail.com>
2020-10-29 11:30:05 +0000

    fixed bug w/ click hanlder again

--- src/components/blocks/facet-box/_facet-box.js
+++ src/components/blocks/facet-box/_facet-box.js
@@ -272,7 +272,7 @@ const initialiseFacetOverlay = () => {
       }));
     }
 
-    if (e.target.parentElement.classList.contains(facetTerm) || e.target.closest(facetTerm)) {
+    if (e.target.parentElement.classList.contains(facetTerm)) {
       const parent = e.target.closest(facetTerm) || e.target.parentElement;
       termList.dispatchEvent(newTermToggleEvent(parent.dataset, false));
       parent.dispatchEvent(newTermToggleEvent(parent.dataset));
