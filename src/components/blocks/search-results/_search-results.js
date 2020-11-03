8f42aa3fee3b2fd31f98319fcb57b968cdf79982
Patrick Cartlidge <patrickcartlidge@gmail.com>
2020-11-03 15:23:11 +0000

    making sure offensive results overlay is working correctly...

--- src/components/blocks/search-results/_search-results.js
+++ src/components/blocks/search-results/_search-results.js
@@ -1,10 +1,11 @@
 const warningHTML = `
     <div class="b-search-results__offensive-warning-text">
       This record uses a term that has since fallen from usage and is now considered offensive. The term is repeated in this record in its original historical context.
+      </br>
+      <button class="b-search-results__offensive-warning-button">
+        View record
+      </button>
     </div>
-    <button class="b-search-results__offensive-warning-button">
-      View record
-    </button>
 `;
 
 const offensiveWarningInitializer = () => {
@@ -13,25 +14,34 @@ const offensiveWarningInitializer = () => {
       const warningEl = document.createElement('DIV');
       warningEl.className = 'b-search-results__offensive-warning';
       warningEl.innerHTML = warningHTML;
-      warningEl.style.top = `${el.offsetTop + (el.offsetHeight / 4)}px`;
-      warningEl.style.left = `${el.getBoundingClientRect().left}px`;
+      warningEl.style.top = `${el.offsetTop + (el.offsetHeight / 5)}px`;
+      warningEl.style.left = `${el.getBoundingClientRect().left / 3}px`;
       warningEl.setAttribute('data-row-index', i);
       warningEl.onclick = (e) => {
-        Array.from(document.querySelectorAll('.b-search-results__body-row'))[e.target.parentElement.getAttribute('data-row-index')].classList.remove('b-search-results__body-row--offensive');
+        Array.from(document.querySelectorAll('.b-search-results__body-row'))[
+          e.target.parentElement.getAttribute('data-row-index')
+        ].classList.remove('b-search-results__body-row--offensive');
         e.target.parentElement.remove();
+        window.dispatchEvent(new Event('resize'));
       };
-      document.querySelector('.b-search-results').appendChild(warningEl);
+      el.appendChild(warningEl);
     }
   });
   window.addEventListener('resize', () => {
+    const table = document.querySelector('.b-search-results__table');
+
     Array.from(document.querySelectorAll('.b-search-results__offensive-warning')).forEach((el) => {
       const row = Array.from(document.querySelectorAll('.b-search-results__body-row'))[el.getAttribute('data-row-index')];
-      el.style.top = `${row.offsetTop + (row.offsetHeight / 4)}px`;
-      if (window.outerWidth < 1200) {
-        el.style.left = `${row.getBoundingClientRect().left}px`;
+      el.style.top = `${row.offsetTop + (row.offsetHeight / 5)}px`;
+
+      if (table.clientWidth < 769) {
+        el.style.left = `5px`;
+      } else {
+        el.style.left = `${el.getBoundingClientRect().left / 5}px`;
       }
     });
   });
 };
 
 offensiveWarningInitializer();
+window.dispatchEvent(new Event('resize'));
