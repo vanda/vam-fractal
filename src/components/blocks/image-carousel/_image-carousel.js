5a8713b679b60e7c20f562982057e65e8dbd0c47
Patrick Cartlidge <patrickcartlidge@gmail.com>
2020-11-13 16:17:20 +0000

    reworked image overlay

--- src/components/blocks/image-carousel/_image-carousel.js
+++ src/components/blocks/image-carousel/_image-carousel.js
@@ -1,133 +1,163 @@
-function scrollcarousel (px) {
-  const currentScroll = document.querySelector('.b-image-carousel__image-carousel').scrollLeft;
-  document.querySelector('.b-image-carousel__image-carousel').scrollLeft = currentScroll + px;
+const imageCarousel = document.querySelector('.b-image-carousel');
+const concealRight = document.querySelector('.b-image-carousel__conceal-right');
+const concealLeft = document.querySelector('.b-image-carousel__conceal-left');
+const prevButton = document.querySelector('.b-image-carousel__prev');
+const nextButton = document.querySelector('.b-image-carousel__next');
+const totalNumberOfImages = document.querySelector('.b-image-overlay-detail__total-number-of-images');
+const imageCounter = document.querySelector('.b-image-overlay-detail__current-image');
+
+let image = document.querySelector('.b-image-overlay__image');
+
+const { images } = imageCarousel ? JSON.parse(imageCarousel.dataset.images) : {};
+
+const imagesWithImage = images.map(({ src, alt }) => {
+  const newImage = new Image();
+  newImage.src = src;
+  newImage.alt = alt;
+  newImage.className = 'b-image-overlay__image b-image-overlay__image--active'
+  return newImage;
+});
+
+const thumbs = images.map(({ thumb, alt }) => {
+  const newImage = new Image();
+  newImage.src = thumb;
+  newImage.alt = `thumbnail for ${alt}`;
+  newImage.className = 'b-image-carousel__image-preview'
+  return newImage;
+});
+
+const changeViewIndex = (index) => {
+  imageCarousel.dataset.viewIndex = (images.length - Math.max(0, index - 2) < 5) ? images.length - 5 : Math.max(0, index - 2);
+}
+const changeIndexAndViewIndex = (index) => {
+  changeViewIndex(index);
+  imageCarousel.dataset.index = index;
 }
 
-function initImageCarousel () {
-  if (document.querySelector('.b-image-carousel')) {
-    const updateImageAndCounterEvent = (i = -1) => {
-      if (i >= 0) {
-        return new CustomEvent('updateimageandcounter', {
-          bubbles: true,
-          detail: {
-            index: i
-          }
-        });
-      }
-      return new CustomEvent('updateimageandcounter', { bubbles: true });
+const initImageCourselContainers = () => {
+  const carouselContainers = Array.from(document.querySelectorAll('.b-image-carousel__image-preview-container'));
+
+  const viewIndex = parseInt(imageCarousel.dataset.viewIndex, 10)
+  const imagesInView = images.slice(viewIndex, viewIndex + 5);
+
+  carouselContainers.forEach((container, i) => {
+    let index = (parseInt(imageCarousel.dataset.viewIndex, 10) + i);
+
+    container.classList.remove('b-image-carousel__image-preview-container--selected');
+
+    if (index === parseInt(imageCarousel.dataset.index)) {
+      container.classList.add('b-image-carousel__image-preview-container--selected');
+    }
+
+    if (container.firstElementChild) {
+      container.firstElementChild.remove();
+    }
+
+    container.appendChild(thumbs[index]);
+
+    container.onclick = (e) => {
+      changeIndexAndViewIndex(index);
     };
+  });
+}
 
-    const scrollPx = 160;
-    const imageCarouselPreview = document.createElement('div');
+const initImageCarousel = () => {
+  totalNumberOfImages.innerHTML = images.length;
 
-    imageCarouselPreview.classList.add('b-image-carousel__image-preview-container');
-    imageCarouselPreview.innerHTML = '<div class="b-image-carousel__image-preview"></div>';
+  if (!imageCarousel.dataset.index) {
+    imageCarousel.dataset.index = 0;
+  }
 
-    Array.from(document.querySelectorAll('.b-image-overlay__image')).forEach(
-      (el, i) => {
-        const cln = imageCarouselPreview.cloneNode(true);
-        cln.setAttribute('data-image-index', i);
-        document.querySelector('.b-image-carousel__image-carousel').appendChild(cln);
-      }
-    );
+  if (!imageCarousel.dataset.viewIndex) {
+    imageCarousel.dataset.viewIndex = 0;
+  }
 
-    if (document.querySelectorAll('.b-image-carousel__image-preview').length > 3) {
-      Array.from(document.querySelectorAll('.b-image-carousel__navigation-container')).forEach(
-        (e) => {
-          e.style.display = 'block';
-        }
-      );
-
-      document.querySelector('.b-image-carousel__conceal-right').style.display = 'block';
-      document.querySelector('.b-image-carousel').classList.add('b-image-carousel--conceal');
+  if (imageCarousel) {
+    for (let i = 0; i < Math.max(0, 5 - images.length); i++) {
+      document.querySelector('.b-image-carousel__image-preview-container').remove();
     }
 
-    Array.from(document.querySelectorAll('.b-image-carousel__image-preview-container')).forEach(
-      (el, i) => {
-        el.onclick = (e) => {
-          e.preventDefault();
-          e.target.dispatchEvent(updateImageAndCounterEvent(i));
-        };
+    initImageCourselContainers();
 
-        if (i === 0) {
-          el.dispatchEvent(updateImageAndCounterEvent(i));
+    const callback = (mutations) => {
+      if (mutations.filter(mutation => mutation.attributeName === 'data-view-index')) {
+        const viewIndex = parseInt(imageCarousel.dataset.viewIndex);
+        concealLeft.style.display = (viewIndex > 0) ? 'block' : 'none';
+        concealRight.style.display = (viewIndex + 5 >= images.length) ? 'none' : 'block';
+      }
+
+      if (mutations.filter(mutation => mutation.attributeName === 'data-index')) {
+        const index = parseInt(imageCarousel.dataset.index);
+        const selectedImg = images[index];
+
+        const newImage = imagesWithImage[index];
+        const imageParent = document.querySelector('.b-image-overlay__figure');
+
+        image.remove();
+
+        imageParent.appendChild(newImage);
+
+        image = newImage;
+        initImageCourselContainers();
+
+        if (index > 0) {
+          prevButton.removeAttribute('disabled');
+        } else {
+          prevButton.setAttribute('disabled', 'true');
         }
 
-        const imgSrc = document.querySelector(`img[data-image-index='${i}']`).getAttribute('src');
+        if (index === images.length - 1) {
+          nextButton.setAttribute('disabled', 'true');
+        } else {
+          nextButton.removeAttribute('disabled');
+        }
 
-        el.querySelector('.b-image-carousel__image-preview').style.background = `url('${imgSrc}')`;
-        el.querySelector('.b-image-carousel__image-preview').style.backgroundSize = 'cover';
+        imageCounter.innerHTML = `${index + 1}`;
       }
-    );
-
-    if (document.querySelector('.b-image-carousel__image-carousel')) {
-      document.querySelector('.b-image-carousel__next').onclick = () => {
-        scrollcarousel(scrollPx);
-      };
-      document.querySelector('.b-image-carousel__prev').onclick = () => {
-        scrollcarousel(-scrollPx);
-      };
     }
 
-    const concealcarousel = document.querySelector('.b-image-carousel--conceal .b-image-carousel__image-carousel');
+    const observer = new MutationObserver(callback);
+    observer.observe(imageCarousel, { attributes: true });
 
-    document.addEventListener('keydown', () => {
-      const currentElementScroll = (parseInt(document.querySelector('.b-image-carousel__image-preview-container--selected').getAttribute('data-image-index'), 10) + 1) * 122;
-      const carouselScroll = document.querySelector('.b-image-carousel__image-carousel').scrollLeft;
-      const carouselWidth = document.querySelector('.b-image-carousel__image-carousel').offsetWidth;
-      if (event.keyCode === 37) {
-        document.querySelector('.b-image-carousel__image-carousel').dispatchEvent(new CustomEvent('updateimageandcounter', { detail: { opr: '-' }, bubbles: true }));
-        if ((currentElementScroll - scrollPx - 122) < carouselScroll) {
-          scrollcarousel(-scrollPx);
-        }
+    imageCarousel.dataset.viewIndex = 0;
+    imageCarousel.dataset.index = 0;
+  }
+
+  document.addEventListener('keydown', () => {
+    if (event.keyCode === 37) {
+      const index = parseInt(imageCarousel.dataset.index, 10) - 1;
+      if (index >= 0) {
+        changeIndexAndViewIndex(index);
       }
-      if (event.keyCode === 39) {
-        document.querySelector('.b-image-carousel__image-carousel').dispatchEvent(new CustomEvent('updateimageandcounter', { detail: { opr: '+' }, bubbles: true }));
-        if ((currentElementScroll + scrollPx) > (carouselScroll + carouselWidth)) {
-          scrollcarousel(scrollPx);
-        }
-      }
-    });
-
-    if (concealcarousel) {
-      const concealLeft = document.querySelector('.b-image-carousel__conceal-left');
-      const concealRight = document.querySelector('.b-image-carousel__conceal-right');
-      const buttonLeft = document.querySelector('.b-image-carousel__prev');
-      const buttonRight = document.querySelector('.b-image-carousel__next');
-
-      concealLeft.onclick = (e) => {
-        e.preventDefault();
-        scrollcarousel(-scrollPx);
-      };
-
-      concealRight.onclick = (e) => {
-        e.preventDefault();
-        scrollcarousel(scrollPx);
-      };
-
-      concealcarousel.addEventListener('scroll', (e) => {
-        const scrollLeft = e.target.scrollLeft;
-        const scrollWidth = e.target.scrollWidth;
-        const width = e.target.offsetWidth;
-
-        /* controlling the buttons */
-        if (scrollLeft === 0) {
-          document.querySelector('.b-image-carousel__conceal-left').style.display = 'none';
-          buttonLeft.classList.remove('b-image-carousel__prev--enabled');
-        } else if (scrollLeft > 0) {
-          document.querySelector('.b-image-carousel__conceal-left').style.display = 'block';
-          buttonLeft.classList.add('b-image-carousel__prev--enabled');
-        }
-        /* controlling the buttons */
-        if (scrollLeft === (scrollWidth - width)) {
-          document.querySelector('.b-image-carousel__conceal-right').style.display = 'none';
-          buttonRight.classList.remove('b-image-carousel__next--enabled');
-        } else if (scrollLeft < (scrollWidth - width)) {
-          document.querySelector('.b-image-carousel__conceal-right').style.display = 'block';
-          buttonRight.classList.add('b-image-carousel__next--enabled');
-        }
-      });
     }
+    if (event.keyCode === 39) {
+     const index = parseInt(imageCarousel.dataset.index, 10) + 1;
+     if (index < images.length) {
+      changeIndexAndViewIndex(index);
+     }
+    }
+  });
+
+  concealRight.onclick = (e) => {
+    const viewIndex = parseInt(imageCarousel.dataset.viewIndex, 10) + 3;
+    changeViewIndex(viewIndex);
+    initImageCourselContainers();
+  };
+
+  concealLeft.onclick = (e) => {
+    const viewIndex = parseInt(imageCarousel.dataset.viewIndex, 10) - 3;
+    changeViewIndex(viewIndex);
+    initImageCourselContainers();
+  };
+
+  prevButton.onclick = (e) => {
+    const index = parseInt(imageCarousel.dataset.index, 10);
+    changeIndexAndViewIndex(index - 1);
+  }
+
+  nextButton.onclick = (e) => {
+    const index = parseInt(imageCarousel.dataset.index, 10);
+    changeIndexAndViewIndex(index + 1);
   }
 }
 
