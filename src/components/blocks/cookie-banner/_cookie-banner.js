2f4e6ce900b1ee0b539376a773601101054da12f
methodog <sittingbottom@gmail.com>
2021-02-11 15:28:03 +0000

    cookie-banner: use eventListener

--- src/components/blocks/cookie-banner/_cookie-banner.js
+++ src/components/blocks/cookie-banner/_cookie-banner.js
@@ -2,11 +2,11 @@ const cookieBanner = document.querySelector('.js-cookie-banner');
 
 if (cookieBanner) {
   const closeButton = document.querySelector('.js-cookie-banner-close');
-  window.onload = () => {
+  document.addEventListener('DOMContentLoaded', () => {
     cookieBanner.classList.remove('b-cookie-banner--hidden');
-  };
-  closeButton.onclick = (event) => {
-    event.preventDefault();
+  }, true);
+  closeButton.onclick = (e) => {
+    e.preventDefault();
     cookieBanner.classList.add('b-cookie-banner--hidden');
   };
 }
