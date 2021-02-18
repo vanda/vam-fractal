84c5fb0cf9ea0a17647c7e52d45d912ac14df1e9
Patrick Cartlidge <patrickcartlidge@gmail.com>
2021-02-18 17:13:45 +0000

    now cannot tab to the objects underneath the oic and esc will close the oic

--- src/components/blocks/object-image-overlay/_object-image-overlay.js
+++ src/components/blocks/object-image-overlay/_object-image-overlay.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-underscore-dangle */
 const oicInit = () => {
   const oicSeeds = Array.from(document.querySelectorAll('.js-object-image-overlay-item'));
+  let focusableElements = [];
 
   if (oicSeeds.length) {
     const oic = document.querySelector('.b-object-image-overlay') || document.createElement('div');
     document.body.appendChild(oic);
     oic.classList.add('b-object-image-overlay');
-    oic.setAttribute('tabindex', 0);
+    // oic.setAttribute('tabindex', 0);
     oic.innerHTML = `
       <button class="b-object-image-overlay__dismiss" title="Close" aria-label="Close"></button>
       <div class="b-object-image-overlay__items"></div>
@@ -54,7 +55,7 @@ const oicInit = () => {
         locationCopy = data.storageOverride || 'This object is currently not on display';
       }
       const visitLink = data && data.visitUrl ?
-        `<a class="b-object-image-overlay__visit" href="${data.visitUrl}" data-tracking-oic="visit the object">Find out how to visit this object</a>`
+        `<a tabindex="-1" class="b-object-image-overlay__visit" href="${data.visitUrl}" data-tracking-oic="visit the object">Find out how to visit this object</a>`
         : '';
       const location = locationCopy || visitLink ?
         `<div class="b-object-image-overlay__location">
@@ -65,10 +66,10 @@ const oicInit = () => {
         ` : '';
       const objectUrl = seed.querySelector('a').getAttribute('href');
       const ctaScreen = objectUrl.length > 1 ?
-        `<br/><a class="b-object-image-overlay__cta b-object-image-overlay__cta--screen" href="${objectUrl}" data-tracking-oic="explore the object">Explore object in more depth</a>`
+        `<br/><a tabindex="-1" class="b-object-image-overlay__cta b-object-image-overlay__cta--screen" href="${objectUrl}" data-tracking-oic="explore the object">Explore object in more depth</a>`
         : '';
       const ctaMobile = objectUrl.length > 1 ?
-        `<a class="b-object-image-overlay__cta b-object-image-overlay__cta--mobile" href="${objectUrl}" data-tracking-oic="explore the object">Explore object in more depth</a>`
+        `<a tabindex="-1" class="b-object-image-overlay__cta b-object-image-overlay__cta--mobile" href="${objectUrl}" data-tracking-oic="explore the object">Explore object in more depth</a>`
         : '';
       const item = document.createElement('div');
       item.classList.add('b-object-image-overlay__item');
@@ -86,13 +87,13 @@ const oicInit = () => {
             <figcaption class="b-object-image-overlay__figcaption">
               ${numberCopyright}
               <div class="b-object-image-overlay__prevnext">
-                <button class="b-object-image-overlay__prev b-object-image-overlay__prev--disabled" title="Previous" aria-label="Previous" data-tracking-oic="previous object">
+                <button disabled class="b-object-image-overlay__prev b-object-image-overlay__prev--disabled" title="Previous" aria-label="Previous" data-tracking-oic="previous object">
                   <svg role="img" viewBox="0 0 100 100">
                     <path fill="none" d="M-1-1h582v402H-1z"/>
                     <path d="M70.173 12.294L57.446.174l-47.62 50 47.62 50 12.727-12.122-36.075-37.879z" fill="currentColor"/>
                   </svg>
                 </button>
-                <button class="b-object-image-overlay__next b-object-image-overlay__next--disabled" title="Next" aria-label="Next" data-tracking-oic="next object">
+                <button disabled class="b-object-image-overlay__next b-object-image-overlay__next--disabled" title="Next" aria-label="Next" data-tracking-oic="next object">
                   <svg role="img" viewBox="0 0 100 100">
                     <path fill="none" d="M-1-1h582v402H-1z"/>
                     <path d="M20 88.052l12.727 12.121 47.62-50-47.62-50L20 12.294l36.075 37.88z" fill="currentColor"/>
@@ -117,15 +118,6 @@ const oicInit = () => {
       } else {
         items.appendChild(item);
       }
-
-      const itemPrev = item.querySelector('.b-object-image-overlay__prev');
-      const itemNext = item.querySelector('.b-object-image-overlay__next');
-      if (index > 0) {
-        itemPrev.classList.add('b-object-image-overlay__prev--enabled');
-      }
-      if (index < oicSeeds.length - 1) {
-        itemNext.classList.add('b-object-image-overlay__next--enabled');
-      }
     };
 
     oic.getIndex = (seed) => {
@@ -144,6 +136,42 @@ const oicInit = () => {
       }
     };
 
+    oic.buttonInit = () => {
+      oic.querySelectorAll('button').forEach(el => el.setAttribute('disabled', true));
+      oic.querySelectorAll('a').forEach(el => el.setAttribute('tabindex', '-1'));
+      oic.querySelector('.b-object-image-overlay__dismiss').removeAttribute('disabled');
+      // this logic needs to be moved here because otherwise the buttons off screen
+      // will be focused by tabbing
+      const item = document.querySelectorAll('.b-object-image-overlay__item')[1];
+      const itemPrev = item.querySelector('.b-object-image-overlay__prev');
+      const itemNext = item.querySelector('.b-object-image-overlay__next');
+      if (oic._index > 0) {
+        itemPrev.classList.add('b-object-image-overlay__prev--enabled');
+        itemPrev.removeAttribute('disabled');
+      }
+      if (oic._index < oicSeeds.length - 1) {
+        itemNext.classList.add('b-object-image-overlay__next--enabled');
+        itemNext.removeAttribute('disabled');
+      }
+      item.querySelectorAll('a').forEach(el => el.removeAttribute('tabindex'));
+
+     const currentItem = Array.from(document.querySelectorAll('.b-object-image-overlay__item'))[1];
+
+      oic.focusable = [
+        document.querySelector('.b-object-image-overlay__dismiss'),
+      ].concat(Array.from(
+        currentItem.querySelectorAll(
+          'button:not([disabled]), [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+        )
+      ));
+
+      oic.focusable = oic.focusable.filter(x => !x.classList.contains('b-object-image-overlay__cta--mobile'));
+
+      oic.focusable[0].focus();
+
+      oic.focusable = oic.focusable.filter(x => !x.classList.contains('b-object-image-overlay__cta--mobile'))
+    }
+
     oic.advance = (rewind = false) => {
       if ((!rewind && oic._index < oicSeeds.length - 1)
         || (rewind && oic._index > 0)) {
@@ -152,6 +180,8 @@ const oicInit = () => {
         oic._index += (1 * (rewind ? -1 : 1));
         oic.track(oic._index);
       }
+
+      oic.buttonInit();
     };
 
     oic.track = (index) => {
@@ -166,7 +196,9 @@ const oicInit = () => {
     document.addEventListener('click', (e) => {
       if (e.target.closest('.js-object-image-overlay-item')) {
         e.preventDefault();
+
         const seed = e.target.closest('.js-object-image-overlay-item');
+
         oic._index = oic.getIndex(seed);
         oic.addItem(oic._index);
         oic.classList.add('b-object-image-overlay--active');
@@ -174,31 +206,71 @@ const oicInit = () => {
         oic.addItem(oic._index - 1, true);
         oic.focus();
         oic.track(oic._index);
+        oic.buttonInit();
+
+        const closeModal = () => {
+          oic.classList.remove('b-object-image-overlay--active');
+          items.innerHTML = '';
+          oic.onclick = null;
+          document.removeEventListener('keydown', keyHandle, false);
+        };
+
+        const currentItem = Array.from(document.querySelectorAll('.b-object-image-overlay__item'))[1];
+
+        // oic.focusable = [
+        //   document.querySelector('.b-object-image-overlay__dismiss'),
+        // ].concat(Array.from(
+        //   currentItem.querySelectorAll(
+        //     'button:not([disabled]), [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+        //   )
+        // ));
+
+        // oic.focusable = oic.focusable.filter(x => !x.classList.contains('b-object-image-overlay__cta--mobile'));
+
+        // oic.focusable[0].focus();
 
         const keyHandle = (e3) => {
           if (e3.key === 'ArrowLeft') {
             e3.preventDefault();
             oic.advance(true);
+            oic.buttonInit();
           } else if (e3.key === 'ArrowRight') {
             e3.preventDefault();
             oic.advance();
+            oic.buttonInit();
+          } else if (e3.key === 'Escape' || e3.key === 'Esc') {
+            closeModal();
+          } else if (e3.keyCode === 9) {
+            const first = oic.focusable[0];
+            const last = oic.focusable[oic.focusable.length - 1];
+            const shift = e3.shiftKey;
+
+            if (oic.focusable.length) {
+              if (shift && document.activeElement === first) {
+                last.focus();
+                e3.preventDefault();
+              } else if (!shift && document.activeElement === last) {
+                first.focus();
+                e3.preventDefault();
+              }
+            }
           }
         };
+
         document.addEventListener('keydown', keyHandle, false);
 
         oic.onclick = (e2) => {
           if (e2.target.matches('.b-object-image-overlay__item, .b-object-image-overlay__dismiss')) {
             e2.preventDefault();
-            oic.classList.remove('b-object-image-overlay--active');
-            items.innerHTML = '';
-            oic.onclick = null;
-            document.removeEventListener('keydown', keyHandle, false);
+            closeModal();
           } else if (e2.target.closest('.b-object-image-overlay__next--enabled')) {
             e2.preventDefault();
             oic.advance();
+            oic.buttonInit();
           } else if (e2.target.closest('.b-object-image-overlay__prev--enabled')) {
             e2.preventDefault();
             oic.advance(true);
+            oic.buttonInit();
           }
         };
       }
