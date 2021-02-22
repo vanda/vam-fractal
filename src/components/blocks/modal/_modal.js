c6092c610c157a13354c8d757b3228437a818abc
Patrick Cartlidge <patrickcartlidge@gmail.com>
2021-02-22 14:55:32 +0000

    cannot tab outside of modal, close is now a link rather than pseudoelement

--- src/components/blocks/modal/_modal.js
+++ src/components/blocks/modal/_modal.js
@@ -13,6 +13,35 @@ function modalTracking (category, action) {
 
 Array.from(modals, (modal) => {
   const modalCampaign = modal.dataset.modalCampaign;
+  const focusable = modal.querySelectorAll('a');
+
+  const focusHandler = (e) => {
+    if (e.keyCode === 9) {
+      const first = focusable[0];
+      const last = focusable[focusable.length - 1];
+      const shift = e.shiftKey;
+      if (focusable.length) {
+        if (shift && document.activeElement === first) {
+          last.focus();
+          e.preventDefault();
+        } else if (!shift && document.activeElement === last) {
+          first.focus();
+          e.preventDefault();
+        }
+      }
+    }
+    if (e.key === 'Escape') {
+      closeModal();
+    }
+  };
+
+  const closeModal = () => {
+    modalTracking(modalCampaign, 'pop-up dismissed');
+    modal.dispatchEvent(new CustomEvent('jsModalClosed', { bubbles: true }));
+    // remove focus focusHandler
+    modal.removeEventListener('keydown', focusHandler);
+    modal.classList.remove('b-modal--active');
+  }
 
   if (
     (!modal.dataset.modalOnceOnly || !cookies.get(modalCampaign)) &&
@@ -21,6 +50,13 @@ Array.from(modals, (modal) => {
     document.body.appendChild(modal);
     modal.classList.add('b-modal--active');
     modalTracking(modalCampaign, 'pop-up displayed');
+
+    // focus the modal instead of the body
+    modal.focus();
+    focusable[1].focus();
+
+    // add focus handler
+    modal.addEventListener('keydown', focusHandler);
   }
 
   modal.addEventListener('click', (e) => {
@@ -31,9 +67,7 @@ Array.from(modals, (modal) => {
       if (!!(Array.from(e.target.classList).find(c => c === 'js-modal-action')) || e.target.closest('.js-modal-action')) {
         modalTracking(modalCampaign, `clicked: ${e.target.textContent}`);
       } else {
-        modalTracking(modalCampaign, 'pop-up dismissed');
-        modal.dispatchEvent(new CustomEvent('jsModalClosed', { bubbles: true }));
-        modal.classList.remove('b-modal--active');
+        closeModal();
       }
     }
   });
