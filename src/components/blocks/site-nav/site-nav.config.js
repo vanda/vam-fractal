a87aad6a65dde6186ba0340dc120c3683cf1ec0b
methodog <sittingbottom@gmail.com>
2022-01-27 11:35:50 +0000

    site-nav: multisite changes

--- src/components/blocks/site-nav/site-nav.config.js
+++ src/components/blocks/site-nav/site-nav.config.js
@@ -3,5 +3,14 @@ module.exports = {
   label: 'Site nav',
   context: {
     title: 'Site nav'
-  }
+  },
+  variants: [
+    {
+      label: 'Solid',
+      name: 'solid',
+      context: {
+        modifiers: ['solid']
+      }
+    }
+  ]
 };
