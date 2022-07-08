2cc0e6b2a4956a632cb59868ed43cc3c9c6fdee1
methodog <sittingbottom@gmail.com>
2022-07-08 14:33:42 +0100

    venue-core-info: variable aspect ratio image [ghi-2602]

--- src/components/blocks/venue-core-info/venue-core-info.config.js
+++ src/components/blocks/venue-core-info/venue-core-info.config.js
@@ -2,6 +2,22 @@ module.exports = {
   title: 'Venue Core Info',
   label: 'Venue Core Info',
   context: {
-    previewClass: 'fr-bg--light'
+    previewClass: 'fr-bg--light',
+    imageSquare: {
+      320: 'https://picsum.photos/id/1040/320/320',
+      640: 'https://picsum.photos/id/1040/640/640',
+      960: 'https://picsum.photos/id/1040/960/960',
+      1280: 'https://picsum.photos/id/1040/1280/1280',
+      1920: 'https://picsum.photos/id/1040/1920/1920',
+      2560: 'https://picsum.photos/id/1040/2560/2560'
+    },
+    imageLandscape: {
+      320: 'https://picsum.photos/id/1040/320/213',
+      640: 'https://picsum.photos/id/1040/640/426',
+      960: 'https://picsum.photos/id/1040/960/640',
+      1280: 'https://picsum.photos/id/1040/1280/853',
+      1920: 'https://picsum.photos/id/1040/1920/1280',
+      2560: 'https://picsum.photos/id/1040/2560/1707'
+    }
   }
 };
