5b9073285a99ff9e1dd0ff4e1412872023c4c14c
methodog <sittingbottom@gmail.com>
2022-09-09 15:36:24 +0100

    object-details: refactor & MS styles [mas-211]

--- src/components/blocks/object-details/object-details.js
+++ src/components/blocks/object-details/object-details.js
@@ -1,16 +1,16 @@
 const revealEl = document.createElement('DIV');
 const classes = [
-  'etc-details__cell-concealer',
-  'etc-details__cell-revealer'
+  'b-object-details__cell-concealer',
+  'b-object-details__cell-revealer'
 ];
 const html = [
-  '<span class="etc-details__cell-concealer-text">Read More</span><button class="etc-details__cell-concealer-button">+</button>',
-  '<span class="etc-details__cell-concealer-text">Read Less</span><button class="etc-details__cell-concealer-button">-</button>'
+  '<span class="b-object-details__cell-concealer-text">Read more</span><button class="b-object-details__cell-concealer-button b-object-details__cell-concealer-button--more"></button>',
+  '<span class="b-object-details__cell-concealer-text">Read less</span><button class="b-object-details__cell-concealer-button"></button>'
 ];
 
 function clickFunction (e) {
-  const hiddenClass = 'etc-details__cell-free-content--hidden';
-  const textEl = e.target.parentElement.querySelector('.etc-details__cell-free-content');
+  const hiddenClass = 'b-object-details__cell-free-content--hidden';
+  const textEl = e.target.parentElement.querySelector('.b-object-details__cell-free-content');
   const textElConcealed = textEl.classList.contains(hiddenClass);
 
   e.target.innerHTML = html[textElConcealed ? 1 : 0];
@@ -30,10 +30,10 @@ function initRevealer () {
   revealEl.innerHTML = html[0];
   revealEl.setAttribute('data-tracking-collections', 'read more');
 
-  Array.from(document.querySelectorAll('.etc-details__cell-free')).forEach((e) => {
+  Array.from(document.querySelectorAll('.b-object-details__cell-free')).forEach((e) => {
     if (e.offsetHeight > 200) {
-      const content = e.querySelector('.etc-details__cell-free-content');
-      content.classList.add('etc-details__cell-free-content--hidden');
+      const content = e.querySelector('.b-object-details__cell-free-content');
+      content.classList.add('b-object-details__cell-free-content--hidden');
       const clone = revealEl.cloneNode(true);
       clone.onclick = clickFunction;
       e.appendChild(clone);
