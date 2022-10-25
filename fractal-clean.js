db49581a606b340f0c45f09893156df19748a630
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2022-10-25 09:34:53 +0100

    Add post-publication clean up script

---
+++ fractal-clean.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+try {
+  const buildDirs = ['www', 'build'];
+
+  for (const dir of buildDirs) {
+    if (fs.existsSync(dir)) {
+      console.log(`Deleting directory: /${dir}`);
+      fs.rmSync(`${dir}`, { recursive: true, force: true });
+    } else {
+      console.log(`Directory: /${dir} does not exist.`);
+    }
+  }
+} catch (error) {
+  console.error('Error in deleting directories.', error);
+}
