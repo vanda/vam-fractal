39da4c9b4bf347b120ebdb4edd5b3ccbce64e4b6
Hussain <h.ali@vam.ac.uk>
2024-02-27 11:17:23 +0000

    Disable submit on landing page if error message is active

--- src/components/blocks/search-form/_search-form.js
+++ src/components/blocks/search-form/_search-form.js
@@ -269,17 +269,23 @@ Array.from(document.querySelectorAll('.js-search-site, .js-search-etc-gateway'),
 
     searchForm.addEventListener('submit', (e) => {
       e.preventDefault();
-
       const formData = new FormData(searchForm);
-      let etcQuery = '';
-      Array.from(formData.entries(), (pair) => {
-        if (pair[0] === 'sel_etc') return true;
-        if (searchFocus.selectedIndex > 0 && pair[0] === 'q') pair[0] = formData.get('sel_etc');
-        etcQuery += (etcQuery.length > 1 ? '&' : '') + `${pair[0]}=${pair[1]}`; // eslint-disable-line prefer-template
-        return true;
-      });
 
-      window.location.href = `${searchForm.action}?${etcQuery}`;
+      // focused search + 3 or more chars
+      // normal search any values.
+      const validSearchCriteria = formData.has('q') || (formData.get('sel_etc').toString().startsWith('q_') && searchInput.value.length > 2);
+
+      if (validSearchCriteria) {
+        let etcQuery = '';
+        Array.from(formData.entries(), (pair) => {
+          if (pair[0] === 'sel_etc') return true;
+          if (searchFocus.selectedIndex > 0 && pair[0] === 'q') pair[0] = formData.get('sel_etc');
+          etcQuery += (etcQuery.length > 1 ? '&' : '') + `${pair[0]}=${pair[1]}`; // eslint-disable-line prefer-template
+          return true;
+        });
+
+        window.location.href = `${searchForm.action}?${etcQuery}`;
+      }
     });
   }
   return true;
