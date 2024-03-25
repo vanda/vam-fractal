8ee866a8bbf9210913f92eacfca77d33ebb77b76
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2024-03-25 15:54:24 +0000

    Alter read more/less controls to be keyboard accessible

--- src/components/blocks/object-details/object-details.js
+++ src/components/blocks/object-details/object-details.js
@@ -1,27 +1,47 @@
+const svgSprite = document.getElementById('vam-svg-sprite-path');
 const revealEl = document.createElement('DIV');
 const classes = [
-  'b-object-details__cell-concealer',
-  'b-object-details__cell-revealer',
+  'b-object-details__cell-control b-object-details__cell-concealer',
+  'b-object-details__cell-control b-object-details__cell-revealer',
 ];
 const html = [
-  '<span class="b-object-details__cell-concealer-text">Read more</span><button class="b-object-details__cell-concealer-button b-object-details__cell-concealer-button--more" aria-label="Read more"></button>',
-  '<span class="b-object-details__cell-concealer-text">Read less</span><button class="b-object-details__cell-concealer-button" aria-label="Read less"></button>',
+  `<button class="b-object-details__cell-concealer-button" aria-label="Read more">
+    <span class="b-object-details__cell-concealer-text">Read more</span>
+    <svg role="presentation" class="b-object-details__cell-concealer-svg">
+      <use xlink:href="${svgSprite.href}#plus"></use>
+    </svg>
+    </button>`,
+  `<button class="b-object-details__cell-concealer-button" aria-label="Read less">
+    <span class="b-object-details__cell-concealer-text">Read less</span>
+    <svg role="presentation" class="b-object-details__cell-concealer-svg">
+      <use xlink:href="${svgSprite.href}#minus"></use>
+    </svg>
+  </button>`,
 ];
 
 function clickFunction(e) {
+  const detailsEl = e.currentTarget.closest('.b-object-details__cell-free');
+  const textEl = detailsEl.querySelector('.b-object-details__cell-free-content');
+  const controlEl = detailsEl.querySelector('.b-object-details__cell-control');
   const hiddenClass = 'b-object-details__cell-free-content--hidden';
-  const textEl = e.target.parentElement.querySelector('.b-object-details__cell-free-content');
   const textElConcealed = textEl.classList.contains(hiddenClass);
 
-  e.target.innerHTML = html[textElConcealed ? 1 : 0];
-  e.target.className = classes[textElConcealed ? 1 : 0];
+  // At this point the click event is lost as the markup is replaced.
+  // Orig. code got round this by placing the event on the <div> but
+  // we want native keyboard accessibility on the <button>
+  controlEl.innerHTML = html[textElConcealed ? 1 : 0];
+  controlEl.className = classes[textElConcealed ? 1 : 0];
+
+  // Get a reference to the replaced <button> and add the recursive event call
+  const btnEl = controlEl.querySelector('.b-object-details__cell-concealer-button');
+  btnEl.addEventListener('click', clickFunction, false);
 
   if (textElConcealed) {
     textEl.classList.remove(hiddenClass);
-    e.target.setAttribute('data-tracking-collections', 'read less');
+    controlEl.setAttribute('data-tracking-collections', 'read less');
   } else {
     textEl.classList.add(hiddenClass);
-    e.target.setAttribute('data-tracking-collections', 'read more');
+    controlEl.setAttribute('data-tracking-collections', 'read more');
   }
 }
 
@@ -34,8 +54,11 @@ function initRevealer() {
     if (e.offsetHeight > 200) {
       const content = e.querySelector('.b-object-details__cell-free-content');
       content.classList.add('b-object-details__cell-free-content--hidden');
+
       const clone = revealEl.cloneNode(true);
-      clone.onclick = clickFunction;
+      const btn = clone.querySelector('.b-object-details__cell-concealer-button');
+      btn.addEventListener('click', clickFunction, false);
+
       e.appendChild(clone);
     }
   });
