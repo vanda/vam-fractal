586bc4f092e1dd5a788366c904a88e8b51869f2e
Miles Lemon <miles@bigbluedoor.net>
2024-04-09 21:53:26 -0700

    [#187227450] remove swiper library and implement simple custom carousel functionality

--- src/components/blocks/carousel/_carousel.js
+++ src/components/blocks/carousel/_carousel.js
@@ -1,16 +1,66 @@
-import Swiper from 'swiper';
-import { EffectFade, Navigation } from 'swiper/modules';
+const container = document.querySelector('.b-carousel__track');
 
-const container = document.querySelector('.b-carousel__swiper');
+if (container) {
+  const slides = container.querySelectorAll('.b-carousel__item');
+  const slideCount = slides.length;
+  let activeSlide = slideCount - 1;
 
-const swiper = new Swiper({
-  modules: [EffectFade, Navigation],
-  loop: true,
-  effect: "fade",
-  navigation: {
-    nextEl: '.b-carousel__next',
-    prevEl: '.b-carousel__prev',
-  },
-});
+  // Loop over each slide and get the 
+  const initCarousel = () => {
+    let greatestHeight = 0;
 
-swiper.init(container);
+    slides.forEach((slide) => {
+      // Reset slide position to get the actual height
+      slide.classList.remove('b-carousel__item--absolute');
+      const slideHeight = slide.offsetHeight;
+
+      if (slideHeight > greatestHeight) {
+        greatestHeight = slideHeight;
+      }
+    });
+
+    // Set container height to that of the tallest slide
+    container.style.height = `${greatestHeight}px`;
+
+    slides.forEach((slide) => {
+      slide.classList.add('b-carousel__item--absolute');
+    });
+
+    // Add active class to the initial slide (last)
+    slides[slides.length - 1].classList.add('b-carousel__item--active');
+  };
+
+  window.addEventListener("resize", () => {
+    initCarousel();
+  });
+
+  initCarousel();
+
+  // Determine which slide to show based on the direction
+  const navigateCarousel = (direction) => {
+    if (direction === 'prev') {
+      activeSlide -= 1;
+
+      if (activeSlide < 0) {
+        activeSlide = slideCount - 1;
+      }
+    } else {
+      activeSlide += 1;
+
+      if (activeSlide >= slideCount) {
+        activeSlide = 0;
+      }
+    }
+
+    slides.forEach((slide) => {
+      slide.classList.remove('b-carousel__item--active');
+    });
+
+    slides[activeSlide].classList.add('b-carousel__item--active');
+  };
+
+  const nextButton = document.querySelector('.u-btn--navigation--next');
+  const prevButton = document.querySelector('.u-btn--navigation--prev');
+  nextButton.addEventListener('click', () => navigateCarousel('next'));
+  prevButton.addEventListener('click', () => navigateCarousel('prev'));
+}
