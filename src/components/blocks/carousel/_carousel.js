23e7ca1d1f46397f74683aff356f6684fbd6764e
Luca Carini <l.carini@vam.ac.uk>
2024-04-17 17:04:05 +0100

    carousel: fix keyboard & screenreader accessibility; add swipe gesture support; change initial item to first instead of last; various other fixes & cleanups

--- src/components/blocks/carousel/_carousel.js
+++ src/components/blocks/carousel/_carousel.js
@@ -1,9 +1,9 @@
-const container = document.querySelector('.b-carousel__track');
+const carousel = document.querySelector('.b-carousel__track');
 
-if (container) {
-  const slides = container.querySelectorAll('.b-carousel__item');
+if (carousel) {
+  const slides = carousel.querySelectorAll('.b-carousel__item');
   const slideCount = slides.length;
-  let activeSlide = slideCount - 1;
+  let activeSlide = 0;
 
   const initCarousel = () => {
     let greatestHeight = 0;
@@ -19,23 +19,24 @@ if (container) {
     });
 
     // Set container height to that of the tallest slide
-    container.style.height = `${greatestHeight}px`;
+    carousel.style.height = `${greatestHeight}px`;
 
     slides.forEach((slide) => {
       slide.classList.add('b-carousel__item--absolute');
+      slide.querySelector('.b-carousel__item-link').setAttribute('tabindex', -1);
     });
 
-    // Add active class to the initial slide (last)
-    slides[slides.length - 1].classList.add('b-carousel__item--active');
+    // Add active class to the initial slide
+    slides[0].classList.add('b-carousel__item--active');
+    slides[0].querySelector('.b-carousel__item-link').removeAttribute('tabindex');
   };
+
+  initCarousel();
 
   window.addEventListener('resize', () => {
     initCarousel();
   });
 
-  initCarousel();
-
-  // Determine which slide to show based on the direction
   const navigateCarousel = (direction) => {
     if (direction === 'prev') {
       activeSlide -= 1;
@@ -53,13 +54,54 @@ if (container) {
 
     slides.forEach((slide) => {
       slide.classList.remove('b-carousel__item--active');
+      slide.querySelector('.b-carousel__item-link').setAttribute('tabindex', -1);
     });
 
     slides[activeSlide].classList.add('b-carousel__item--active');
+    slides[activeSlide].querySelector('.b-carousel__item-link').removeAttribute('tabindex');
   };
 
-  const nextButton = document.querySelector('.u-btn--navigation--next');
-  const prevButton = document.querySelector('.u-btn--navigation--prev');
-  nextButton.addEventListener('click', () => navigateCarousel('next'));
-  prevButton.addEventListener('click', () => navigateCarousel('prev'));
+  document.addEventListener('click', (e) => {
+    if (e.target.closest('.b-carousel__navigation-prev')) {
+      navigateCarousel('prev');
+    } else if (e.target.closest('.b-carousel__navigation-next')) {
+      navigateCarousel('next');
+    }
+  });
+
+  const keyHandle = (e) => {
+    if (document.activeElement.closest('.b-carousel__item')) {
+      if (e.key === 'ArrowLeft') {
+        navigateCarousel('prev');
+      } else if (e.key === 'ArrowRight') {
+        navigateCarousel('next');
+      }
+    } else if (document.activeElement.closest('[class^="b-carousel__navigation"]')
+    && e.key === 'Enter') {
+      // on keyboard navigation using prev/next buttons, move focus to newly selected item,
+      // otherwise tabbing sequence is broken (unavoidable consequence of design)
+      if (document.activeElement.classList.contains('b-carousel__navigation-prev')) {
+        navigateCarousel('prev');
+      } else {
+        navigateCarousel('next');
+      }
+      slides[activeSlide].querySelector('.b-carousel__item-link').focus();
+    }
+  };
+  document.addEventListener('keydown', keyHandle);
+
+  carousel.ontouchstart = (e) => {
+    const startXY = [e.touches[0].pageX, e.touches[0].pageY];
+    carousel.ontouchmove = (e2) => {
+      const deltaXY = [e2.touches[0].pageX - startXY[0], e2.touches[0].pageY - startXY[1]];
+      if (Math.abs(deltaXY[0]) > Math.abs(deltaXY[1])) {
+        carousel.ontouchmove = null;
+        if (deltaXY[0] > 0) {
+          navigateCarousel('prev');
+        } else {
+          navigateCarousel('next');
+        }
+      }
+    };
+  };
 }
--- src/components/blocks/carousel/carousel.config.js
+++ src/components/blocks/carousel/carousel.config.js
@@ -7,60 +7,44 @@ module.exports = {
     ctaLabel: 'View all',
     carouselItems: [
       {
-        title: 'Featured content heading',
+        title: '#1 Featured content heading',
         description: 'Description of featured content',
-        url: 'https://www.vam.ac.uk/exhibitions/pink-floyd',
+        url: 'https://www.vam.ac.uk/test-1',
         image: {
-          320: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/16/12/27/07/8462b6bc-d141-4808-8af2-cb5caeddfd50/leman_hero.jpg',
-          640: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/16/12/27/07/8462b6bc-d141-4808-8af2-cb5caeddfd50/leman_hero.jpg',
-          960: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/16/12/27/07/8462b6bc-d141-4808-8af2-cb5caeddfd50/leman_hero.jpg',
-          1280: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/16/12/27/07/8462b6bc-d141-4808-8af2-cb5caeddfd50/leman_hero.jpg',
-        },
+          320: 'https://vanda-production-assets.s3.amazonaws.com/2019/05/15/09/26/40/ef8b1e1a-c8e9-420a-b278-a80fe2010c83/320.jpg',
+          640: 'https://vanda-production-assets.s3.amazonaws.com/2019/05/15/09/26/40/d63b33f5-9500-416a-9d5d-1714015fc097/640.jpg',
+          960: 'https://vanda-production-assets.s3.amazonaws.com/2019/05/15/09/26/40/65179005-be92-4292-a387-62fb15c5d888/960.jpg',
+          1280: 'https://vanda-production-assets.s3.amazonaws.com/2019/05/15/09/26/40/f5128c47-1d15-4869-910a-dcbb1ed7378e/1280.jpg',
+          1920: 'https://vanda-production-assets.s3.amazonaws.com/2019/05/15/09/26/40/8e244833-8b78-4701-9b77-2639312dbf94/1920.jpg',
+          2560: 'https://vanda-production-assets.s3.amazonaws.com/2019/05/15/09/26/41/7dfc2083-58d3-4928-ba07-3cf428eadad6/2560.jpg'
+        }
       },
       {
-        title: 'The Leman Album: an enhanced facsimile',
-        description: 'This 18th-century volume contains Europe\'s earliest dated silk designs – close to 100 astonishingly vibrant drawings on paper.',
-        url: 'https://www.vam.ac.uk/exhibitions/pink-floyd',
-        image: {
-          320: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/21/14/02/29/2d07df00-6dc7-4bbb-9061-b94ded76d107/vari_collections.jpg',
-          640: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/21/14/02/29/2d07df00-6dc7-4bbb-9061-b94ded76d107/vari_collections.jpg',
-          960: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/21/14/02/29/2d07df00-6dc7-4bbb-9061-b94ded76d107/vari_collections.jpg',
-          1280: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/21/14/02/29/2d07df00-6dc7-4bbb-9061-b94ded76d107/vari_collections.jpg',
-        },
-      },
-      {
-        title: 'Featured content heading',
+        title: '#2 Featured content heading',
         description: 'Description of featured content',
-        url: 'https://www.vam.ac.uk/exhibitions/pink-floyd',
+        url: 'https://www.vam.ac.uk/test-2',
         image: {
-          320: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/16/12/27/57/dd738a02-b547-4fed-8e7d-4eb6e145caa6/cabinet_curiosities_hero.jpg',
-          640: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/16/12/27/57/dd738a02-b547-4fed-8e7d-4eb6e145caa6/cabinet_curiosities_hero.jpg',
-          960: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/16/12/27/57/dd738a02-b547-4fed-8e7d-4eb6e145caa6/cabinet_curiosities_hero.jpg',
-          1280: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/16/12/27/57/dd738a02-b547-4fed-8e7d-4eb6e145caa6/cabinet_curiosities_hero.jpg',
-        },
+          320: 'https://vanda-production-assets.s3.amazonaws.com/2016/04/29/10/51/59/e2d419a2-bf64-4fa6-92e1-01cca82a835e/Blue-glass-form-2560.jpg',
+          640: 'https://vanda-production-assets.s3.amazonaws.com/2016/04/29/10/52/00/dd2b8dff-3285-44c9-bc73-f33e16d071ab/Blue-glass-form-2560.jpg',
+          960: 'https://vanda-production-assets.s3.amazonaws.com/2016/04/29/10/52/00/85ca285a-f4c9-446a-8a55-ab2065f81133/Blue-glass-form-2560.jpg',
+          1280: 'https://vanda-production-assets.s3.amazonaws.com/2016/04/29/10/52/00/abe97dab-83fc-4633-8e80-5b04f6a82d9d/Blue-glass-form-2560.jpg',
+          1920: 'https://vanda-production-assets.s3.amazonaws.com/2016/04/29/10/52/00/ef71bb86-fc36-428f-b2a9-80e56a89df70/Blue-glass-form-2560.jpg',
+          2560: 'https://vanda-production-assets.s3.amazonaws.com/2016/04/29/10/52/00/5ee4b571-2911-4eef-8574-4cd13e0474ee/Blue-glass-form-2560.jpg'
+        }
       },
       {
-        title: 'The Leman Album: an enhanced facsimile',
-        description: 'This 18th-century volume contains Europe\'s earliest dated silk designs – close to 100 astonishingly vibrant drawings on paper.',
-        url: 'https://www.vam.ac.uk/exhibitions/pink-floyd',
-        image: {
-          320: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/21/14/11/26/5249f3f0-7307-48c8-9804-e9d7a5f7347a/vari_digital.jpg',
-          640: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/21/14/11/26/5249f3f0-7307-48c8-9804-e9d7a5f7347a/vari_digital.jpg',
-          960: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/21/14/11/26/5249f3f0-7307-48c8-9804-e9d7a5f7347a/vari_digital.jpg',
-          1280: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/21/14/11/26/5249f3f0-7307-48c8-9804-e9d7a5f7347a/vari_digital.jpg',
-        },
-      },
-      {
-        title: 'Featured content heading',
+        title: '#3 Featured content heading',
         description: 'Description of featured content',
-        url: 'https://www.vam.ac.uk/exhibitions/pink-floyd',
+        url: 'https://www.vam.ac.uk/test-3',
         image: {
-          320: 'https://vanda-production-assets.s3.amazonaws.com/2017/05/22/10/20/08/8cdb6539-2e9a-4a74-bdf0-a43cb7402f12/vari_residency.jpg',
-          640: 'https://vanda-production-assets.s3.amazonaws.com/2017/05/22/10/20/08/8cdb6539-2e9a-4a74-bdf0-a43cb7402f12/vari_residency.jpg',
-          960: 'https://vanda-production-assets.s3.amazonaws.com/2017/05/22/10/20/08/8cdb6539-2e9a-4a74-bdf0-a43cb7402f12/vari_residency.jpg',
-          1280: 'https://vanda-production-assets.s3.amazonaws.com/2017/05/22/10/20/08/8cdb6539-2e9a-4a74-bdf0-a43cb7402f12/vari_residency.jpg',
-        },
-      },
+          320: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/11/00e4d6c7-df8a-4bac-928e-002837524fe1/320.jpg',
+          640: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/11/ce6a17c4-dfb2-4dad-9d36-9e92187e2ad1/640.jpg',
+          960: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/11/c72bd651-e26f-4765-8f70-764a0e274c55/960.jpg',
+          1280: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/12/d73d930a-42a8-4acb-ac5f-6c03561bbfa6/1280.jpg',
+          1920: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/12/c8d09cd6-c361-460f-a388-f2118ee44c1d/1920.jpg',
+          2560: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/12/4f6d683f-854e-4576-a0dd-6f54c78ae7b0/2560.jpg'
+        }
+      }
     ]
   }
 };
