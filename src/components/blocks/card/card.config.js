c7b5498a8927d08f65b61468b403f56b1383a32e
Luca Carini <l.carini@vam.ac.uk>
2024-04-17 17:10:09 +0100

    supply proper full image srcset, needed for testing responsive img tag

--- src/components/blocks/card/card.config.js
+++ src/components/blocks/card/card.config.js
@@ -5,10 +5,12 @@ module.exports = {
     cardTitle: 'The Leman Album: an enhanced facsimile',
     cardDescription: 'This 18th-century volume contains Europe\'s earliest dated silk designs – close to 100 astonishingly vibrant drawings on paper.',
     cardImage: {
-      320: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/16/12/27/07/8462b6bc-d141-4808-8af2-cb5caeddfd50/leman_hero.jpg',
-      640: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/16/12/27/07/8462b6bc-d141-4808-8af2-cb5caeddfd50/leman_hero.jpg',
-      960: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/16/12/27/07/8462b6bc-d141-4808-8af2-cb5caeddfd50/leman_hero.jpg',
-      1280: 'https://vanda-production-assets.s3.amazonaws.com/2017/03/16/12/27/07/8462b6bc-d141-4808-8af2-cb5caeddfd50/leman_hero.jpg',
+      320: 'https://framemark.vam.ac.uk/collections/2020MP2543/full/320,/0/default.jpg',
+      640: 'https://framemark.vam.ac.uk/collections/2020MP2543/full/640,/0/default.jpg',
+      960: 'https://framemark.vam.ac.uk/collections/2020MP2543/full/960,/0/default.jpg',
+      1280: 'https://framemark.vam.ac.uk/collections/2020MP2543/full/1280,/0/default.jpg',
+      1920: 'https://framemark.vam.ac.uk/collections/2020MP2543/full/1920,/0/default.jpg',
+      2560: 'https://framemark.vam.ac.uk/collections/2020MP2543/full/2560,/0/default.jpg'
     },
   },
   variants: [
