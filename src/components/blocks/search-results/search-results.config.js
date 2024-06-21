5e6a01e0de48ba563867e094e8bcc106810b5223
Luca Carini <ldcarini@gmail.com>
2024-06-21 16:59:03 +0100

    Revert "Merge stc2-860-etc-search-results-object-card-changes"

--- src/components/blocks/search-results/search-results.config.js
+++ src/components/blocks/search-results/search-results.config.js
@@ -11,54 +11,42 @@ module.exports = {
             title: 'A Vase That Looks Neat With A Really Long Title',
             artist: 'Reknowned Vase Maker',
             date: 'late 18th century',
-            site: 'Young V&A',
-            location: 'Play gallery',
-            status: 'On display'
+            place: 'Britain Britain Britain Britain Britain Britain, Britain Britain Britain Britain Britain'
           },
           {
             image: 'https://place-hold.it/150x71',
             title: 'A Vase That Looks Neat',
             artist: 'Reknowned Vase Maker',
             date: 'late 18th century',
-            site: 'Young V&A',
-            location: 'Play gallery',
-            status: 'Request to view'
+            place: 'Britain'
           },
           {
             image: 'https://place-hold.it/82x170',
             title: 'Cool Print',
             artist: 'Established Printer',
             date: '1980',
-            site: 'V&A South Kensington',
-            location: 'Prints & Drawings Study Room, level D',
-            status: 'Not on display'
+            place: 'Germany'
           },
           {
             image: 'https://place-hold.it/150x71',
-            title: 'Wardrobe',
-            artist: 'Berman, Eugène',
-            date: '1939',
-            site: 'V&A South Kensington',
-            location: 'Furniture, Room 133, The Dr Susan Weber Gallery',
-            status: 'On display'
+            title: 'Ancient Spoon',
+            artist: 'Unknown',
+            date: '200 AD',
+            place: 'Rome'
           },
           {
             image: 'https://place-hold.it/150x106',
-            title: 'Antarctic Expedition',
-            artist: 'Maskrey, James',
-            date: '2013',
-            site: 'V&A South Kensington',
-            location: 'Contemporary Glass, Room 129, The Märit Rausing Gallery',
-            status: 'On display'
+            title: 'Flappy Bird',
+            artist: 'Dong Nguyen',
+            date: '2014',
+            place: 'Vietnam'
           },
           {
             image: 'none',
-            title: 'Ancient book of wonders',
+            title: 'Newly Acquired Object',
             artist: 'Unknown',
             date: '2020',
-            site: 'V&A South Kensington',
-            location: 'National Art Library',
-            status: 'Not on display'
+            place: 'Japan'
           }
         ]
       }
@@ -75,9 +63,6 @@ module.exports = {
             artist: 'Reknowned Vase Maker',
             date: 'late 18th century',
             place: 'Britain',
-            site: 'V&A South Kensington',
-            location: 'Ceramics, Room 142, The Lydia and Manfred Gorvy Gallery',
-            status: 'On display',
             offensive: true
           },
           {
@@ -85,19 +70,14 @@ module.exports = {
             title: 'A Vase That Looks Neat',
             artist: 'Reknowned Vase Maker',
             date: 'late 18th century',
-            place: 'Britain',
-            site: 'V&A South Kensington',
-            location: 'British Galleries, Room 118, The Wolfson Gallery',
-            status: 'Request to view'
+            place: 'Britain'
           },
           {
             image: 'https://place-hold.it/82x170',
             title: 'Cool Print',
             artist: 'Established Printer',
             date: '1980',
-            site: 'V&A South Kensington',
-            location: 'British Galleries, Room 122',
-            status: 'Not on display',
+            place: 'Germany',
             offensive: true
           },
           {
@@ -105,27 +85,21 @@ module.exports = {
             title: 'Ancient Spoon',
             artist: 'Unknown',
             date: '200 AD',
-            site: 'Young V&A',
-            location: 'Play gallery',
-            status: 'Not on display'
+            place: 'Rome'
           },
           {
             image: 'https://place-hold.it/150x106',
             title: 'Flappy Bird',
             artist: 'Dong Nguyen',
             date: '2014',
-            site: 'Young V&A',
-            location: 'Play gallery',
-            status: 'On display'
+            place: 'Vietnam'
           },
           {
             image: 'none',
             title: 'Newly Acquired Object',
             artist: 'Unknown',
             date: '2020',
-            site: 'V&A South Kensington',
-            location: 'Halfway up the stairs',
-            status: 'On display'
+            place: 'Japan'
           }
         ]
       }
