e7402a33ccb3ac85f1e3e9a5933cde5bab3fcc46
Luca Carini <l.carini@vam.ac.uk>
2024-07-25 17:56:29 +0100

    search-results: rework

--- src/components/blocks/search-results/search-results.config.js
+++ src/components/blocks/search-results/search-results.config.js
@@ -1,141 +1,37 @@
 module.exports = {
-  variants: [
-    {
-      name: 'default',
-      label: 'Default',
-      context: {
-        pages: 10,
-        items: [
-          {
-            image: 'https://place-hold.it/150x71',
-            title: 'A Vase That Looks Neat With A Really Long Title',
-            artist: 'Reknowned Vase Maker',
-            date: 'late 18th century',
-            site: 'Young V&A',
-            location: 'Play gallery',
-            status: 'On display'
-          },
-          {
-            image: 'https://place-hold.it/150x71',
-            title: 'A Vase That Looks Neat',
-            artist: 'Reknowned Vase Maker',
-            date: 'late 18th century',
-            site: 'Young V&A',
-            location: 'Play gallery',
-            status: 'Request to view'
-          },
-          {
-            image: 'https://place-hold.it/82x170',
-            title: 'Cool Print',
-            artist: 'Established Printer',
-            date: '1980',
-            site: 'V&A South Kensington',
-            location: 'Prints & Drawings Study Room, level D',
-            status: 'Not on display'
-          },
-          {
-            image: 'https://place-hold.it/150x71',
-            title: 'Wardrobe',
-            artist: 'Berman, Eugène',
-            date: '1939',
-            site: 'V&A South Kensington',
-            location: 'Furniture, Room 133, The Dr Susan Weber Gallery',
-            status: 'On display'
-          },
-          {
-            image: 'https://place-hold.it/150x106',
-            title: 'Antarctic Expedition',
-            artist: 'Maskrey, James',
-            date: '2013',
-            site: 'V&A South Kensington',
-            location: 'Contemporary Glass, Room 129, The Märit Rausing Gallery',
-            status: 'On display'
-          },
-          {
-            image: 'none',
-            title: 'Ancient book of wonders',
-            artist: 'Unknown',
-            date: '2020',
-            site: 'V&A South Kensington',
-            location: 'National Art Library',
-            status: 'Not on display'
-          }
-        ]
+  title: 'Search EtC Row',
+  label: 'Search EtC Row',
+  context: {
+    previewClass: 'fr-bg--etc',
+    items: [
+      {
+        image: 'https://framemark.vam.ac.uk/collections/2023NP9019',
+        link: 'https://collections.vam.ac.uk/item/O1225457',
+        caption: 'In the Alps',
+        captionSub: ['Eric Powell', '1900-1930'],
+        locationSite: 'V&A South Kensington',
+        locationRoom: 'Prints & Drawings Study Room, level D',
+        locationType: 'Request to view'
+      },
+      {
+        image: 'https://framemark.vam.ac.uk/collections/2023NP7712',
+        link: 'https://collections.vam.ac.uk/item/O1274909/',
+        caption: 'Isella Simplon Pass, and more words to show how a long title is handled at various widths',
+        captionSub: ['Seide', 'ca. 1800-1869'],
+        locationSite: 'V&A South Kensington',
+        locationRoom: 'Prints & Drawings Study Room, level D',
+        locationType: 'Request to view'
+      },
+      {
+        offensive: true,
+        image: 'https://framemark.vam.ac.uk/collections/2023NP6551',
+        link: 'https://collections.vam.ac.uk/item/O1181579/',
+        caption: 'Alps near Samplons',
+        captionSub: ['Muirhead Bone', '1911'],
+        locationSite: 'V&A South Kensington',
+        locationRoom: 'Prints & Drawings Study Room, level D',
+        locationType: 'Request to view'
       }
-    },
-    {
-      name: 'Offensive',
-      label: 'Offensive',
-      context: {
-        pages: 10,
-        items: [
-          {
-            image: 'https://place-hold.it/150x71',
-            title: 'A Vase That Looks Neat',
-            artist: 'Reknowned Vase Maker',
-            date: 'late 18th century',
-            place: 'Britain',
-            site: 'V&A South Kensington',
-            location: 'Ceramics, Room 142, The Lydia and Manfred Gorvy Gallery',
-            status: 'On display',
-            offensive: true
-          },
-          {
-            image: 'https://place-hold.it/150x71',
-            title: 'A Vase That Looks Neat',
-            artist: 'Reknowned Vase Maker',
-            date: 'late 18th century',
-            place: 'Britain',
-            site: 'V&A South Kensington',
-            location: 'British Galleries, Room 118, The Wolfson Gallery',
-            status: 'Request to view'
-          },
-          {
-            image: 'https://place-hold.it/82x170',
-            title: 'Cool Print',
-            artist: 'Established Printer',
-            date: '1980',
-            site: 'V&A South Kensington',
-            location: 'British Galleries, Room 122',
-            status: 'Not on display',
-            offensive: true
-          },
-          {
-            image: 'https://place-hold.it/150x71',
-            title: 'Ancient Spoon',
-            artist: 'Unknown',
-            date: '200 AD',
-            site: 'Young V&A',
-            location: 'Play gallery',
-            status: 'Not on display'
-          },
-          {
-            image: 'https://place-hold.it/150x106',
-            title: 'Flappy Bird',
-            artist: 'Dong Nguyen',
-            date: '2014',
-            site: 'Young V&A',
-            location: 'Play gallery',
-            status: 'On display'
-          },
-          {
-            image: 'none',
-            title: 'Newly Acquired Object',
-            artist: 'Unknown',
-            date: '2020',
-            site: 'V&A South Kensington',
-            location: 'Halfway up the stairs',
-            status: 'On display'
-          }
-        ]
-      }
-    },
-    {
-      name: 'search-has-no-results',
-      label: 'has no results',
-      context: {
-        items: []
-      }
-    }
-  ]
+    ]
+  }
 };
