ff11071aa335a6b7737cd1c01a8a9edf1d43b1f6
Luca Carini <l.carini@vam.ac.uk>
2024-08-02 14:20:19 +0100

    object-card: rework

--- src/components/blocks/object-card/_object-card.js
+++ src/components/blocks/object-card/_object-card.js
@@ -1,24 +1,31 @@
 export default (() => {
-  Array.from(document.querySelectorAll('.b-object-card-warning'), (card) => {
+  Array.from(document.querySelectorAll('.b-object-card-warning'), (card, i) => {
+    const cardId = `object-card-${i}`;
+    card.id = cardId;
+    card.setAttribute('aria-hidden', 'true');
+    Array.from(card.querySelectorAll('a, button'), (el) => el.setAttribute('tabindex', '-1'));
     card.innerHTML = `
-      <button class="b-object-card-warning__banner"
-        role="comment" id="${card.getAttribute('aria-details')}">
-        <div class="b-object-card-warning__banner-text">
-          This object, or the text that describes it, is deemed offensive and discriminatory. We are committed to improving our records, and work is ongoing.
-        </div>
-        <div class="b-object-card-warning__banner-dismiss" aria-hidden="true">
-          View record
-        </div>
-      </button>
-      ${card.innerHTML}
+    <button class="b-object-card-warning__banner" aria-expanded="false" aria-controls="${cardId}">
+    <div class="b-object-card-warning__banner-text">
+    This object, or the text that describes it, is deemed offensive and discriminatory. We are committed to improving our records, and work is ongoing.
+    </div>
+    <div class="b-object-card-warning__banner-dismiss">
+    View record
+    </div>
+    </button>
+    ${card.innerHTML}
     `;
     return true;
   });
 
   document.addEventListener('click', (e) => {
     if (e.target.closest('.b-object-card-warning')) {
-      e.target.closest('.b-object-card-warning').classList.remove('b-object-card-warning');
-      e.target.remove();
+      const card = e.target.closest('.b-object-card-warning');
+      card.classList.remove('b-object-card-warning');
+      Array.from(card.querySelectorAll('[tabindex="-1"]'), (el) => el.removeAttribute('tabindex'));
+      card.removeAttribute('aria-hidden');
+      card.firstElementChild.remove();
+      card.querySelector('a').focus();
     }
   });
 });
