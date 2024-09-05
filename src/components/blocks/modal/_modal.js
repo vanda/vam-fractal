001e36144c92dcc61d5b6d0701417593b680405c
Luca Carini <l.carini@vam.ac.uk>
2024-09-05 12:39:32 +0100

    modal: minor improvements

--- src/components/blocks/modal/_modal.js
+++ src/components/blocks/modal/_modal.js
@@ -13,33 +13,32 @@ function modalTracking(category, action) {
 
 Array.from(modals, (modal) => {
   const modalCampaign = modal.dataset.modalCampaign == null ? 'modalCampaign-not-set' : modal.dataset.modalCampaign;
-  const focusable = modal.querySelectorAll('a');
 
   const closeModal = () => {
     modal.setAttribute('tabindex', '-1');
     modal.setAttribute('aria-hidden', 'true');
     modalTracking(modalCampaign, 'pop-up dismissed');
     modal.dispatchEvent(new CustomEvent('jsModalClosed', { bubbles: true }));
-    // remove focus focusHandler
     modal.classList.remove('b-modal--active');
+    modal.removeEventListener('keydown', focusHandler); // eslint-disable-line no-use-before-define
   };
 
   const focusHandler = (e) => {
     if (e.key === 'Tab') {
+      const focusable = modal.querySelectorAll('a, button:enabled, input:enabled');
       const first = focusable[0];
       const last = focusable[focusable.length - 1];
       const shift = e.shiftKey;
       if (focusable.length) {
         if (shift && document.activeElement === first) {
+          e.preventDefault();
           last.focus();
-          e.preventDefault();
         } else if (!shift && document.activeElement === last) {
-          first.focus();
           e.preventDefault();
+          first.focus();
         }
       }
-    }
-    if (e.key === 'Escape') {
+    } else if (e.key === 'Escape') {
       modal.removeEventListener('keydown', focusHandler);
       closeModal();
     }
@@ -55,9 +54,8 @@ Array.from(modals, (modal) => {
     modal.classList.add('b-modal--active');
     modalTracking(modalCampaign, 'pop-up displayed');
 
-    // focus the modal instead of the body
-    modal.focus();
-    focusable[1].focus();
+    // apply focus to modal
+    modal.querySelector('a, button:enabled, input:enabled').focus();
 
     // add focus handler
     modal.addEventListener('keydown', focusHandler);
@@ -67,13 +65,10 @@ Array.from(modals, (modal) => {
     if (modal.dataset.modalOnceOnly) {
       cookies.set(modalCampaign, 'seen', { domain: modal.dataset.modalDomain, expires: 365 });
     }
-    if (e.target !== modal) {
-      if (!!(Array.from(e.target.classList).find((c) => c === 'js-modal-action')) || e.target.closest('.js-modal-action')) {
-        modalTracking(modalCampaign, `clicked: ${e.target.textContent}`);
-      } else {
-        modal.removeEventListener('keydown', focusHandler);
-        closeModal();
-      }
+    if (e.target.closest('.js-modal-action')) {
+      modalTracking(modalCampaign, `clicked: ${e.target.textContent}`);
+    } else if (e.target.closest('.js-modal-exit')) {
+      closeModal();
     }
   });
   return true;
