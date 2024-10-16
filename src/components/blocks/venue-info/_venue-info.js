b87c0903e351870107d76819970c1e5ab0517466
Luca Carini <l.carini@vam.ac.uk>
2024-10-16 09:35:45 +0100

    venue-info: as carousel - rework - restrict focus action to non-active items, improving animation  alignment

--- src/components/blocks/venue-info/_venue-info.js
+++ src/components/blocks/venue-info/_venue-info.js
@@ -2,48 +2,54 @@ import { scrollIntoViewHorizontally } from '../../services/js_utility_functions/
 
 document.addEventListener('DOMContentLoaded', () => {
   Array.from(document.querySelectorAll('.b-venue-info'), (component) => {
-    Array.from(component.querySelectorAll('.b-venue-info__items'), (carousel) => {
-      const items = carousel.querySelectorAll('.b-venue-info__item');
+    const items = component.querySelectorAll('.b-venue-info__item');
 
-      /* Carousel logic only relvant for > 1 item */
-      if (items.length > 1) {
-        /* scroll item into view when it receives focus
-        * requires item to be a focusable element
-        * only focusin event is delegated to container (focus event doesn't bubble!) */
-        carousel.addEventListener('focusin', (e) => {
-          if (e.target.closest('.b-venue-info__item')) {
-            const item = e.target.closest('.b-venue-info__item');
-            const scrollMode = window.getComputedStyle(carousel).getPropertyValue('overflow') !== 'hidden';
+    /* Carousel logic only relvant for > 1 item */
+    if (items.length > 1) {
+      const carousel = component.querySelector('.b-venue-info__items');
+      items[0].classList.add('js-venue-info__item--active');
 
-            if (scrollMode) {
-              /* if native scrolling is enabled by CSS, focus item using native scrolling */
-              scrollIntoViewHorizontally(item);
-            } else {
-              /* else focus item using CSS
-              * only necessary due to browsers refusing to scroll items already within viewport
-              * also affords css control over the animation */
-              const index = Array.prototype.indexOf.call(items, item);
-              const itemShift = index * (items[1].offsetLeft - (carousel.getBoundingClientRect().right - items[1].getBoundingClientRect().left)); // eslint-disable-line max-len
-              component.style.setProperty('--items-offset', `-${itemShift}px`); console.log(component.style.getPropertyValue('--items-offset'));
-            }
+      /* function for setting an item as the only active item */
+      const setActive = (item) => {
+        carousel.querySelector('.js-venue-info__item--active').classList.remove('js-venue-info__item--active');
+        item.classList.add('js-venue-info__item--active');
+      };
+
+      /* scroll item into view when it receives focus
+      * requires item to be a focusable element
+      * only focusin event is delegated to container (focus event doesn't bubble!) */
+      carousel.addEventListener('focusin', (e) => {
+        if (e.target.closest('.b-venue-info__item:not(.js-venue-info__item--active)')) {
+          const item = e.target.closest('.b-venue-info__item');
+          setActive(item);
+          const scrollMode = window.getComputedStyle(carousel).getPropertyValue('overflow') !== 'hidden';
+
+          if (scrollMode) {
+            /* if native scrolling is enabled by CSS, focus item using native scrolling */
+            scrollIntoViewHorizontally(item);
+          } else {
+            /* else focus item using CSS
+            * only necessary due to browsers refusing to scroll items already within viewport
+            * also affords css control over the animation */
+            const index = Array.prototype.indexOf.call(items, item);
+            const itemShift = index * (items[0].offsetLeft + items[0].offsetWidth - (carousel.getBoundingClientRect().right - items[index].getBoundingClientRect().left)); // eslint-disable-line max-len
+            component.style.setProperty('--items-offset', `-${itemShift}px`);
           }
-        });
+        }
+      });
 
-        /* reset native scroll position in case resizing between native/non-native scroll modes */
-        window.addEventListener('resize', () => {
-          if (window.getComputedStyle(carousel).getPropertyValue('overflow') === 'hidden') {
-            carousel.scrollLeft = 0;
-          }
-        });
+      /* reset native scroll position in case resizing between native/non-native scroll modes */
+      window.addEventListener('resize', () => {
+        if (window.getComputedStyle(carousel).getPropertyValue('overflow') === 'hidden') {
+          carousel.scrollLeft = 0;
+        }
+      });
 
-        /* prev/next buttons */
-        const ctrls = component.querySelector('.b-venue-info__ctrls');
-        // only active if multiple items
-        ctrls.classList.add('b-venue-info__ctrls--active');
-      }
-
-      return true;
-    });
+      /* prev/next buttons */
+      const ctrls = component.querySelector('.b-venue-info__ctrls');
+      // only active if multiple items
+      ctrls.classList.add('b-venue-info__ctrls--active');
+    }
 
     return true;
   });
