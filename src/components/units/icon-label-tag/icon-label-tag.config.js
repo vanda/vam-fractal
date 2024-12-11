15fd0e87266972c8034b3cdfa9845fb05e14fc38
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2024-12-11 12:21:18 +0000

    Add new icon-label-tag unit component

---
+++ src/components/units/icon-label-tag/icon-label-tag.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: 'Icon label tag',
+  default: 'attention',
+  context: {
+    previewClass: 'fr-bg--dark',
+    modifier: ['attention'],
+    text: 'Attention',
+    icon: 'attention'
+  },
+  variants: [
+    {
+      name: 'Make',
+      context: {
+        modifier: ['make'],
+        text: 'Make',
+        icon: 'make'
+      }
+    },
+  ]
+}
