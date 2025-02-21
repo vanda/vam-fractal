e3cce1adf91f9e88a2280882a6260eb42a4340cb
Luca Carini <l.carini@vam.ac.uk>
2025-02-21 19:22:35 +0000

    toggle-nav: optimise event listening with event delegation

--- src/components/blocks/toggle-nav/_toggle-nav.js
+++ src/components/blocks/toggle-nav/_toggle-nav.js
@@ -1,50 +1,45 @@
-Array.from(document.querySelectorAll('.js-toggle-nav'), (toggleNav) => {
-  const toggleNavBtns = toggleNav.querySelectorAll('.js-toggle-nav-btn');
+document.addEventListener('click', (e) => {
+  const toggleNav = e.target.closest('.b-toggle-nav');
+  if (toggleNav) {
+    const tog = e.target;
 
-  Array.from(toggleNavBtns, (tog) => {
-    const tnToggees = document.querySelectorAll(tog.dataset.toggeesSelector);
-    const isDarkVariation = tog.classList.contains('u-btn--pill-dark');
+    /* scroll button to centre */
+    toggleNav.scrollLeft = tog.offsetLeft
+      - ((toggleNav.getBoundingClientRect().width - tog.getBoundingClientRect().width) / 2);
 
-    tog.addEventListener('click', (e) => {
-      e.preventDefault();
-      const togType = tog.dataset.toggleType;
-
-      /* scroll button to centre */
-      toggleNav.scrollLeft = tog.offsetLeft
-        - ((toggleNav.getBoundingClientRect().width - tog.getBoundingClientRect().width) / 2);
-
-      Array.from(tnToggees, (el) => {
-        el.style.display = 'none';
-        if (el.dataset.toggleType === togType) {
-          el.style.display = 'block';
-        }
-        return true;
-      });
-
-      Array.from(toggleNavBtns, (el) => {
-        if (isDarkVariation) {
-          el.classList.add('u-btn--pill-dark');
-        } else {
-          el.classList.add('u-btn--pill-light');
-          el.classList.remove('u-btn--pill-darkest');
-        }
-        return true;
-      });
-
-      if (isDarkVariation) {
-        tog.classList.remove('u-btn--pill-dark');
-      } else {
-        tog.classList.remove('u-btn--pill-light');
-        tog.classList.add('u-btn--pill-darkest');
+    /* show/hide related toggleable element */
+    const togType = tog.dataset.toggleType;
+    const togees = document.querySelectorAll(tog.dataset.togeesSelector);
+    Array.from(togees, (el) => {
+      el.style.display = 'none';
+      if (el.dataset.toggleType === togType) {
+        el.style.display = 'block';
       }
+      return true;
     });
 
-    return true;
-  });
-
-  if ('toggleTypeDefault' in toggleNav.dataset) {
-    document.querySelector(`[data-toggle-type=${toggleNav.dataset.toggleTypeDefault}]`).click();
+    const toggleNavBtns = toggleNav.querySelectorAll('.js-toggle-nav-btn');
+    const isDarkVariation = tog.classList.contains('u-btn--pill-dark');
+    Array.from(toggleNavBtns, (el) => {
+      if (isDarkVariation) {
+        el.classList.add('u-btn--pill-dark');
+      } else {
+        el.classList.add('u-btn--pill-light');
+        el.classList.remove('u-btn--pill-darkest');
+      }
+      return true;
+    });
+    if (isDarkVariation) {
+      tog.classList.remove('u-btn--pill-dark');
+    } else {
+      tog.classList.remove('u-btn--pill-light');
+      tog.classList.add('u-btn--pill-darkest');
+    }
   }
+});
 
+/* auto click any toogleNav's default toggle, if set */
+Array.from(document.querySelectorAll('.js-toggle-nav[data-toggle-type-default]'), (toggleNav) => {
+  toggleNav.querySelector(`[data-toggle-type=${toggleNav.dataset.toggleTypeDefault}]`).click();
   return true;
 });
