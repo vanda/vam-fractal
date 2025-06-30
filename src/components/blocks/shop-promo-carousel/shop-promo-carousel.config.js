22d2e52aab298739072677d433a5eb776cab1385
Hussain <h.ali@vam.ac.uk>
2025-06-30 15:27:43 +0100

    Shop promo carousel component added

---
+++ src/components/blocks/shop-promo-carousel/shop-promo-carousel.config.js
@@ -0,0 +1,173 @@
+module.exports = {
+  title: 'Shop Tout',
+  label: 'Shop Tout',
+  default: '3 products',
+  variants: [
+    {
+      name: 'Shop product carousel',
+      context: {
+        promoitems: [
+          {
+            image: {
+              320: 'https://www.vam.ac.uk/dw/image/v2/BDFC_PRD/on/demandware.static/-/Sites-VAM_MasterShop/default/dwa3aee1a0/pdp-images/products/169894-V&A-EAST-GREEN-TOTE.jpg?sw=320&sh=320',
+              640: 'https://www.vam.ac.uk/dw/image/v2/BDFC_PRD/on/demandware.static/-/Sites-VAM_MasterShop/default/dwa3aee1a0/pdp-images/products/169894-V&A-EAST-GREEN-TOTE.jpg?sw=640&sh=640',
+            },
+            title: 'V&A East tote bag',
+            description: 'Inspired by V&A East Museum and Storehouse, this all-new tote bag celebrates the bold creativity of East London.',
+            price: '£15.00',
+            url: 'https://www.vam.ac.uk/shop/fashion/bags-totes/va-east-tote-bag-169894.html'
+          },
+          {
+            image: {
+              320: 'https://www.vam.ac.uk/dw/image/v2/BDFC_PRD/on/demandware.static/-/Sites-VAM_MasterShop/default/dwc512d139/pdp-images/products/160609.jpg?sw=320&sh=320',
+              640: 'https://www.vam.ac.uk/dw/image/v2/BDFC_PRD/on/demandware.static/-/Sites-VAM_MasterShop/default/dwc512d139/pdp-images/products/160609.jpg?sw=640&sh=640',
+            },
+            title: 'V&A building magnet',
+            description: 'This magnet features a design for the decoration of the South Kensington Museum by Godfrey Sykes and Francis Fowke.',
+            price: '£15.00',
+            url: 'https://www.vam.ac.uk/shop/homeware/home-accessories/va-building-magnet-160609.html'
+          },
+          {
+            image: {
+              320: 'https://www.vam.ac.uk/dw/image/v2/BDFC_PRD/on/demandware.static/-/Sites-VAM_MasterShop/default/dw5a1014c7/pdp-images/products/168699%20V&A%20SK%20GUIDEBOOK.png?sw=320&sh=320',
+              640: 'https://www.vam.ac.uk/dw/image/v2/BDFC_PRD/on/demandware.static/-/Sites-VAM_MasterShop/default/dw5a1014c7/pdp-images/products/168699%20V&A%20SK%20GUIDEBOOK.png?sw=640&sh=640',
+            },
+            title: 'V&A South Kensington Guidebook',
+            description: 'V&A South Kensington is a world of extraordinary global creativity with unmissable exhibitions and experiences for all. ',
+            price: '£6.00',
+            url: 'https://www.vam.ac.uk/shop/books/about-the-museum/va-south-kensington-guidebook-168699.html'
+          },
+          {
+            image: {
+              320: 'https://www.vam.ac.uk/dw/image/v2/BDFC_PRD/on/demandware.static/-/Sites-VAM_MasterShop/default/dw739f609d/pdp-images/products/148365.jpg?sw=320&sh=320',
+              640: 'https://www.vam.ac.uk/dw/image/v2/BDFC_PRD/on/demandware.static/-/Sites-VAM_MasterShop/default/dw739f609d/pdp-images/products/148365.jpg?sw=640&sh=640',
+            },
+            title: 'Designing the V&A: The Museum as a Work of Art (1857-1909)',
+            description: 'V&A South Kensington is a world of extraordinary global creativity with unmissable exhibitions and experiences for all. ',
+            price: '£35.00',
+            url: 'https://www.vam.ac.uk/shop/books/all-books/designing-the-va-the-museum-as-a-work-of-art-1857-1909-148365.html'
+          },
+          {
+            image: {
+              320: 'https://www.vam.ac.uk/dw/image/v2/BDFC_PRD/on/demandware.static/-/Sites-VAM_MasterShop/default/dw61b258a1/pdp-images/products/156753.jpg?sw=320&sh=320',
+              640: 'https://www.vam.ac.uk/dw/image/v2/BDFC_PRD/on/demandware.static/-/Sites-VAM_MasterShop/default/dw61b258a1/pdp-images/products/156753.jpg?sw=640&sh=640',
+            },
+            title: 'Grey Houndstooth tote bag',
+            description: 'The V&A\'s fashion collection spans five centuries and is the largest and most comprehensive collection of dress in the world. This pattern design is inspired by the techniques, materials or designs found in this diverse collection.',
+            price: '£8.50',
+            url: 'https://www.vam.ac.uk/shop/fashion/bags-totes/grey-houndstooth-tote-bag-156753.html'
+          },
+          {
+            image: {
+              320: 'https://www.vam.ac.uk/dw/image/v2/BDFC_PRD/on/demandware.static/-/Sites-VAM_MasterShop/default/dw8ed14ee7/pdp-images/products/160455.jpg?sw=320&sh=320',
+              640: 'https://www.vam.ac.uk/dw/image/v2/BDFC_PRD/on/demandware.static/-/Sites-VAM_MasterShop/default/dw8ed14ee7/pdp-images/products/160455.jpg?sw=640&sh=640',
+            },
+            title: 'V&A Lavender soap',
+            description: 'This beautiful bar of luxury soap makes a perfect little gift or treat for the home. Breathe deep and relax with the soothing scent of lavender. Both bar and paper wrapper are UK-made, exclusively for the V&A, with a design adapted from a furnishing fabric produced by George Haité, c.1903.',
+            price: '£5.00',
+            url: 'https://www.vam.ac.uk/shop/homeware/home-fragrance/va-lavender-soap-160455.html'
+          },
+
+        ]
+      }
+    },
+    {
+      name: '1 product',
+      context: {
+        promoitems: [
+          {
+            image: {
+              320: 'https://via.placeholder.com/540x600',
+              640: 'https://via.placeholder.com/540x600',
+              960: 'https://via.placeholder.com/540x600'
+            },
+            title: 'Product',
+            description: 'blah blah',
+            price: '£200.00',
+            url: 'https://www.google.com/shop'
+          }
+        ]
+      }
+    },
+    {
+      name: '2 products',
+      context: {
+        promoitems: [
+          {
+            image: {
+              320: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/11/00e4d6c7-df8a-4bac-928e-002837524fe1/320.jpg',
+              640: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/11/ce6a17c4-dfb2-4dad-9d36-9e92187e2ad1/640.jpg',
+              960: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/11/c72bd651-e26f-4765-8f70-764a0e274c55/960.jpg',
+              1280: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/12/d73d930a-42a8-4acb-ac5f-6c03561bbfa6/1280.jpg',
+              1920: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/12/c8d09cd6-c361-460f-a388-f2118ee44c1d/1920.jpg',
+              2560: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/12/4f6d683f-854e-4576-a0dd-6f54c78ae7b0/2560.jpg'
+            },
+            title: 'Product',
+            description: 'blah blah',
+            price: '£200.00',
+            url: 'https://www.google.com/shop'
+          },
+          {
+            image: {
+              320: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/11/00e4d6c7-df8a-4bac-928e-002837524fe1/320.jpg',
+              640: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/11/ce6a17c4-dfb2-4dad-9d36-9e92187e2ad1/640.jpg',
+              960: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/11/c72bd651-e26f-4765-8f70-764a0e274c55/960.jpg',
+              1280: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/12/d73d930a-42a8-4acb-ac5f-6c03561bbfa6/1280.jpg',
+              1920: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/12/c8d09cd6-c361-460f-a388-f2118ee44c1d/1920.jpg',
+              2560: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/12/4f6d683f-854e-4576-a0dd-6f54c78ae7b0/2560.jpg'
+            },
+            title: 'Product',
+            description: 'blah blah',
+            price: '£200.00',
+            url: 'https://www.google.com/shop'
+          }
+        ]
+      }
+    }
+  ],
+  context: {
+    previewClass: 'fr-bg--dark',
+    title: 'Shop Heading',
+    promoitems: [
+      {
+        image: {
+          320: 'https://via.placeholder.com/540x600',
+          640: 'https://via.placeholder.com/540x600',
+          960: 'https://via.placeholder.com/540x600'
+        },
+        title: 'Product',
+        description: 'blah blah',
+        price: '£200.00',
+        url: 'https://www.google.com/shop'
+      },
+      {
+        image: {
+          320: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/11/00e4d6c7-df8a-4bac-928e-002837524fe1/320.jpg',
+          640: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/11/ce6a17c4-dfb2-4dad-9d36-9e92187e2ad1/640.jpg',
+          960: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/11/c72bd651-e26f-4765-8f70-764a0e274c55/960.jpg',
+          1280: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/12/d73d930a-42a8-4acb-ac5f-6c03561bbfa6/1280.jpg',
+          1920: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/12/c8d09cd6-c361-460f-a388-f2118ee44c1d/1920.jpg',
+          2560: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/12/4f6d683f-854e-4576-a0dd-6f54c78ae7b0/2560.jpg'
+        },
+        title: 'Product',
+        description: 'blah blah',
+        price: '£200.00',
+        url: 'https://www.google.com/shop'
+      },
+      {
+        image: {
+          320: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/11/00e4d6c7-df8a-4bac-928e-002837524fe1/320.jpg',
+          640: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/11/ce6a17c4-dfb2-4dad-9d36-9e92187e2ad1/640.jpg',
+          960: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/11/c72bd651-e26f-4765-8f70-764a0e274c55/960.jpg',
+          1280: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/12/d73d930a-42a8-4acb-ac5f-6c03561bbfa6/1280.jpg',
+          1920: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/12/c8d09cd6-c361-460f-a388-f2118ee44c1d/1920.jpg',
+          2560: 'https://vanda-production-assets.s3.amazonaws.com/2018/01/31/16/36/12/4f6d683f-854e-4576-a0dd-6f54c78ae7b0/2560.jpg'
+        },
+        title: 'Product',
+        description: 'blah blah',
+        price: '£200.00',
+        url: 'https://www.google.com/shop'
+      }
+    ]
+  }
+};
