720abf3076b33158af98f8ae03913a6b1efcc2b5
Luca Carini <l.carini@vam.ac.uk>
2025-10-30 15:54:09 +0000

    breadcrumb: add breadcrumb component

---
+++ src/components/blocks/breadcrumb/breadcrumb.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Breadcrumb',
+  label: 'Breadcrumb',
+  context: {
+    previewClass: 'fr-padded'
+  },
+  variants: [
+    {
+      name: 'On Dark',
+      label: 'On Dark',
+      context: {
+        previewClass: 'fr-padded fr-bg--etc',
+        lightDarkTheme: 's-light-dark-theme--dark'
+      }
+    }
+  ]
+};
