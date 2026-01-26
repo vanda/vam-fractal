771f831c596826fb8681c8b119c7e3dc979d4e3a
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2026-01-26 16:00:29 +0000

    Add new record card component
    Co-authored-by: Luca Carini <l.carini@vam.ac.uk>

---
+++ src/components/blocks/record-card/record-card.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'Record Card',
+  context: {
+    previewClass: 'fr-bg--dark fr-content-edge-space',
+    image: {
+      320: 'https://picsum.photos/id/25/320/240',
+      640: 'https://picsum.photos/id/25/640/480',
+    },
+    heading: 'Vivien Leigh Archive',
+    date: '1925-1967',
+    reference: 'THM/433',
+    extent: '90 Boxes',
+  }
+};
