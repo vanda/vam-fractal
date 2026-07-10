be987cd3a7d5ac5061da166a7bd16e6bec670df2
Luca Carini <l.carini@vam.ac.uk>
2026-07-10 21:46:18 +0100

    rovingTabindex: add utility fn to improve keboard navigation around long lists

--- src/components/services/js_utility_functions/js_utility_functions.js
+++ src/components/services/js_utility_functions/js_utility_functions.js
@@ -14,6 +14,78 @@ const scrollIntoViewHorizontally = (el, container = el.parentElement, visualCont
   }
 };
 
+
+/* rovingTabindex
+ * To improve keyboard navigation around long lists of Links and avoid Tab navigation traps
+ * by switching to Arrow key navigation within long lists of links
+ * using a roving tabindex to manage keyboard focus
+ * https://www.w3.org/WAI/ARIA/apg/practices/keyboard-interface/#kbd_roving_tabindex */
+const rovingTabindex = (el) => {
+  /* Remove all links in the list from the tabindex
+   * leaving one link tabbable to allow tabbing into the list.
+   * the tabbable link will be the first, by default,
+   * unless a particular link has already been set as tabbable */
+  const defaultTab = el.querySelector('a[tabindex="0"]');
+  el._rovingTabindex_links = [...el.querySelectorAll('a')];
+  el._rovingTabindex_links.forEach((link) => link.setAttribute('tabindex', -1));
+  if (defaultTab) defaultTab.setAttribute('tabindex', 0);
+  else el._rovingTabindex_links[0].setAttribute('tabindex', 0);
+
+  /* Add alternative keyboard navigation for traversing within the list,
+   * which moves the focus accordingly,
+   * and sets the newly focussed link to be the only tabbable el in the list.
+   * Ensure listener only added once, even when applied repeatedly to rebuilt lists. */
+  if (!el._rovingTabindex_hasListener) {
+    el.addEventListener('keydown', (e) => {
+      const currentItem = el.querySelector('a[tabindex="0"]');
+      const currentIndex = el._rovingTabindex_links.findIndex((link) => link === currentItem);
+
+      const moveFocus = (nextItem) => {
+        currentItem.setAttribute('tabindex', -1);
+        nextItem.setAttribute('tabindex', 0);
+        nextItem.focus();
+      };
+
+      switch (e.key) {
+        case 'ArrowRight':
+        case 'ArrowDown':
+          /* Move focus to next link,
+          * preventing default scrolling which will happen as necessary with the change of focus */
+          e.preventDefault();
+          moveFocus(el._rovingTabindex_links[currentIndex + 1] || el._rovingTabindex_links[currentIndex]); // eslint-disable-line max-len
+          break;
+
+        case 'ArrowLeft':
+        case 'ArrowUp':
+          /* Move focus to previous link,
+          * preventing default scrolling which will happen as necessary with the change of focus */
+          e.preventDefault();
+          moveFocus(el._rovingTabindex_links[currentIndex - 1] || el._rovingTabindex_links[currentIndex]); // eslint-disable-line no-lonely-if, max-len
+          break;
+
+        case 'Home':
+        case 'PageUp':
+          /* Move focus to first link,
+          * preventing default scrolling which will happen as necessary with the change of focus */
+          e.preventDefault();
+          moveFocus(el._rovingTabindex_links[0]);
+          break;
+
+        case 'End':
+        case 'PageDown':
+          /* Move focus to last link,
+          * preventing default scrolling which will happen as necessary with the change of focus */
+          e.preventDefault();
+          moveFocus(el._rovingTabindex_links[el._rovingTabindex_links.length - 1]);
+          break;
+      }
+    });
+    el._rovingTabindex_hasListener = true;
+  }
+};
+
+
 /* export individual utility functions here as they are added.
  * none are default exports */
-export { scrollIntoViewHorizontally }; // eslint-disable-line import/prefer-default-export
+/* eslint-disable import/prefer-default-export */
+export { scrollIntoViewHorizontally, rovingTabindex };
